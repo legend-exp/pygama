e6b67fa1e8104cb215452101054d205958fc29f5
Clint Wiseman <wisecg.neontetra@gmail.com>
2019-01-22 21:54:00 -0800

    add a cool draw feature

--- pygama/utils.py
+++ pygama/utils.py
@@ -64,3 +64,27 @@ def sh(cmd, sh=False):
     import subprocess as sp
     if not sh: sp.call(shlex.split(cmd))  # "safe"
     else: sp.call(cmd, shell=sh)  # "less safe"
+
+def TDraw(tree, vars, tcut):
+    """
+    if you have to debase yourself and use ROOT, this is an easy
+    convenience function for quickly extracting data from TTrees.
+    TTree::Draw can only handle groups of 4 variables at a time,
+    but here we can put in as many as we want, and
+    return a list of numpy.ndarrays for each one
+    """
+    var_list = vars.split(":")
+    np_arrs = [[] for v in var_list]
+
+    for i in range(0, len(var_list), 4):
+
+        tmp_list = var_list[i:i+4]
+        tmp_draw = ":".join(tmp_list)
+        n = tree.Draw(tmp_draw, tcut, "goff")
+
+        for j, var in enumerate(tmp_list):
+            # print(i, j, var, "getting V", j+1, "writing to np_arrs", i + j)
+            tmp = getattr(tree, "GetV{}".format(j+1))()
+            np_arrs[i + j] = np.array([tmp[k] for k in range(n)])
+
+    return tuple(np_arrs)
