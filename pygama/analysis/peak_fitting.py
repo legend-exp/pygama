6e772ed1d82389adc3e54f00292907d2e3e2b569
jasondet <jasondet@gmail.com>
2019-03-25 14:39:53 -0700

    replace clint's nice changes to the radford peak shape function that I accidentally dropped in the last commit. Jason

--- pygama/analysis/peak_fitting.py
+++ pygama/analysis/peak_fitting.py
@@ -119,9 +119,8 @@ def neg_poisson_log_like(pars, func, hist, bins, **kwargs):
     """
     mu = func(pgh.get_bin_centers(bins), *pars, **kwargs) * pgh.get_bin_widths(bins)
     return np.sum(mu - hist*np.log(mu))
-<<<<<<< HEAD
 
-#Define a gaussian distribution and corresponding neg log likelihood
+
 def gauss(x, mu, sigma, A=1):
     """
     define a gaussian distribution, w/ args: mu, sigma, area (optional).
@@ -135,17 +134,24 @@ def radford_peak(x, mu, sigma, hstep, htail, tau, bg0, a=1):
     David Radford's HPGe peak shape function
     """
     #make sure the fractional amplitude parameters stay reasonable...
-    if htail < 0 or htail > 1: return np.zeros_like(x)
-    if hstep < 0 or hstep > 1: return np.zeros_like(x)
+    if htail < 0 or htail > 1: 
+        return np.zeros_like(x)
+    if hstep < 0 or hstep > 1: 
+        return np.zeros_like(x)
 
     bg_term = bg0  #+ x*bg1
-    if np.any(bg_term < 0): return np.zeros_like(x)
-    step = a * hstep * erfc((x - mu) / (sigma * np.sqrt(2)))
-    le_tail = a * htail * erfc(
-        (x - mu) / (sigma * np.sqrt(2)) + sigma / (tau * np.sqrt(2))) * np.exp(
-            (x - mu) / tau) / (2 * tau * np.exp(-(sigma /
-                                                  (np.sqrt(2) * tau))**2))
-    return (1 - htail) * gauss(x, mu, sigma, a) + bg_term + step + le_tail
+    if np.any(bg_term < 0): 
+        return np.zeros_like(x)
+
+    # compute the step and the low energy tail
+    step = a * hstep * erfc((x - mu) / (sig * np.sqrt(2)))
+    le_tail = h * htail
+    le_tail *= erfc((x - mu) / (sig * np.sqrt(2)) + sig / (tau * np.sqrt(2)))
+    le_tail *= np.exp((x - mu) / tau)
+    le_tail /= (2 * tau * np.exp(-(sig / (np.sqrt(2) * tau))**2))
+
+    # add up all the peak shape components
+    return (1 - htail) * gauss(x, mu, sig, a) + bg_term + step + le_tail
 
 
 # power-law tail plus gaussian https://en.wikipedia.org/wiki/Crystal_Ball_function
