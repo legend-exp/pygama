4b7bfee28fcbdcb9f52f0f3943e181a42c31d07c
wisecg <wisecg.neontetra@gmail.com>
2019-11-23 16:32:48 -0800

    add numpy array io

--- cygama/bindings.cpp
+++ cygama/bindings.cpp
@@ -1,10 +1,10 @@
 #include <pybind11/pybind11.h>
-// #include "test.h"
+#include "test.h"
 #include "sigcompress.h"
 
 namespace py = pybind11;
 
-PYBIND11_PLUGIN(cygama) {
+PYBIND11_MODULE(cygama, m) {
   /*
   structure taken from here:
   https://www.benjack.io/2018/02/02/python-cpp-revisited.html
@@ -12,19 +12,15 @@ PYBIND11_PLUGIN(cygama) {
   TODO: can include sphinx-style docstrings for the c++ functions here, check the example above
   */
   
-  py::module m("cygama", "docstring 1");
+  // m("cygama", "docstring 1");
 
-  // m.def("add", &add, "docstring 2");
+  m.def("add", &add, "docstring 2");
 
-  // m.def("subtract", &subtract, "docstring 3");
+  m.def("subtract", &subtract, "docstring 3");
   
-  // m.def("compress_signal", &compress_signal, "docstring 4");
+  m.def("compress_signal", &compress_signal, "docstring 4");
   
-  // m.def("decompress_signal", &decompress_signal, "docstring 5");
+  m.def("decompress_signal", &decompress_signal, "docstring 5");
   
-  // m.def("multiply", &multiply, "docstring 6");
-  
-  // m.def("py_multiply", &py_multiply, "docstring 6");
-
-  return m.ptr();
+  m.def("multiply", &py_multiply, "docstring 6");
 }
--- cygama/sigcompress.h
+++ cygama/sigcompress.h
@@ -6,17 +6,17 @@
 // Copyright (c) 2018, David C. Radford <radforddc@ornl.gov>
 
 #include <iostream>
+
 #include <vector>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
-
-// namespace py = pybind11;
+namespace py = pybind11;
 
 int compress_signal(short *sig_in, unsigned short *sig_out, int sig_len_in);
 
 int decompress_signal(unsigned short *sig_in, short *sig_out, int sig_len_in);
 
-// std::vector<int> multiply(const std::vector<double>& input);
-// 
-// py::array_t<int> py_multiply(py::array_t<double, py::array::c_style | py::array::forcecast> array);
+std::vector<int> multiply(const std::vector<double>& input);
+
+py::array_t<int> py_multiply(py::array_t<double, py::array::c_style | py::array::forcecast> array);
