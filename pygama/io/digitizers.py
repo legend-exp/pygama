4c0ffc6b02b395e0c8ba9883bdb69f0e538cb71b
jasondet <jasondet@gmail.com>
2020-01-29 23:20:56 -0800

    Add ORCA SIS card readout using dataframes based buffers. Jason

--- pygama/io/digitizers.py
+++ pygama/io/digitizers.py
@@ -72,7 +72,7 @@ class ORCAStruck3302(DataTaker):
             "waveform": { 
               'dtype': 'uint16', 
               #'max_length': 65532, # suggest to override this before calling prepare_buffer() to save RAM
-              'max_length': 8192, # suggest to override this before calling prepare_buffer() to save RAM
+              'shape': (8192,),
               'sample_period_ns': 10, # override if a different clock rate is used
               'units': 'adc',
             },
@@ -169,7 +169,7 @@ class ORCAStruck3302(DataTaker):
             else:
                 len1 = istop_1-i_start_1
                 tbwf[:len1] = p16[i_start_1:i_stop_1]
-                len1 = istop_2-i_start_2
+                len2 = istop_2-i_start_2
                 tbwf[len1:len1+len2] = p16[i_start_2:i_stop_2]
                 tb.set_flat_buffer_len('waveform', len1+len2)
 
@@ -184,6 +184,95 @@ class ORCAStruck3302(DataTaker):
         self.ievt += 1
 
         tb.next_row()
+
+    def decode_packet_to_df_buf(self, packet, df, ii, packet_id, header_dict, verbose=False):
+        """
+        see README for the 32-bit data word diagram
+        """
+
+        # interpret the raw event data into numpy arrays of 16 and 32 bit ints
+        # does not copy data. p32 and p16 are read-only
+        p32 = np.frombuffer(packet, dtype=np.uint32)
+        p16 = np.frombuffer(packet, dtype=np.uint16)
+
+        df.packet_id.values[ii] = packet_id
+
+        # start reading the binary, baby
+        n_lost_msb = (p32[0] >> 25) & 0x7F
+        n_lost_lsb = (p32[0] >> 2) & 0x7F
+        n_lost_records = (n_lost_msb << 7) + n_lost_lsb
+        df.crate.values[ii] = (p32[0] >> 21) & 0xF
+        df.card.values[ii] = (p32[0] >> 16) & 0x1F
+        df.channel.values[ii] = (p32[0] >> 8) & 0xFF
+        buffer_wrap = p32[0] & 0x1
+        #crate_card_chan = (df.crate.values[ii] << 9) + (df.card.values[ii] << 4) + df.channel.values[ii]
+        wf_length32 = p32[1]
+        ene_wf_length32 = p32[2]
+        evt_header_id = p32[3] & 0xFF
+        df.timestamp.values[ii] = ((p32[3] >> 16) & 0xFFFF) << 32 + p32[4]
+        last_word = p32[-1]
+
+        # get the footer
+        df.energy.values[ii] = p32[-4]
+        df.energy_first.values[ii] = p32[-3]
+        extra_flags = p32[-2]
+
+        # compute expected and actual array dimensions
+        wf_length16 = 2 * wf_length32
+        orca_helper_length16 = 2
+        sis_header_length16 = 12 if buffer_wrap else 8
+        header_length16 = orca_helper_length16 + sis_header_length16
+        ene_wf_length16 = 2 * ene_wf_length32
+        footer_length16 = 8
+        expected_wf_length = len(p16) - orca_helper_length16 - sis_header_length16 - \
+            footer_length16 - ene_wf_length16
+
+        # error check: waveform size must match expectations
+        if wf_length16 != expected_wf_length or last_word != 0xdeadbeef:
+            print(len(p16), orca_helper_length16, sis_header_length16,
+                  footer_length16)
+            print("ERROR: Waveform size %d doesn't match expected size %d." %
+                  (wf_length16, expected_wf_length))
+            print("       The Last Word (should be 0xdeadbeef):",
+                  hex(last_word))
+            exit()
+
+        # indexes of stuff (all referring to the 16 bit array)
+        i_wf_start = header_length16
+        i_wf_stop = i_wf_start + wf_length16
+        i_ene_start = i_wf_stop + 1
+        i_ene_stop = i_ene_start + ene_wf_length16
+        if buffer_wrap:
+            # start somewhere in the middle of the record
+            i_start_1 = p32[6] + header_length16 + 1
+            i_stop_1 = i_wf_stop  # end of the wf record
+            i_start_2 = i_wf_start  # beginning of the wf record
+            i_stop_2 = i_start_1
+
+        # handle the waveform(s)
+        energy_wf = np.zeros(ene_wf_length16)  # not used rn
+        dfwf = df.waveform.values[ii]
+        if wf_length32 > 0:
+            if not buffer_wrap:
+                if i_wf_stop - i_wf_start != expected_wf_length:
+                    print("ERROR: event %d, we expected %d WF samples and only got %d" %
+                          (ievt, expected_wf_length, i_wf_stope - i_wf_start))
+                dfwf[:] = p16[i_wf_start:i_wf_stop]
+            else:
+                len1 = istop_1-i_start_1
+                len2 = istop_2-i_start_2
+                if len1+len2 != expected_wf_length:
+                    print("ERROR: event %d, we expected %d WF samples and only got %d" %
+                          (ievt, expected_wf_length, len1+len2))
+                    exit()
+                dfwf[:len1] = p16[i_start_1:i_stop_1]
+                dfwf[len1:len1+len2] = p16[i_start_2:i_stop_2]
+
+
+        # set the event number (searchable HDF5 column)
+        df.ievt.values[ii] = self.ievt
+        self.ievt += 1
+
 
     def decode_event(self, event_data_bytes, packet_id, header_dict, verbose=False):
         """
