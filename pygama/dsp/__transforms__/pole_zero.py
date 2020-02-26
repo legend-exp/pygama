ecf91a751b569b707ef3978eb3ae9252019451e8
Ian <iguinn@uw.edu>
2020-02-26 13:45:09 -0500

    Fixed pole-zero correction and added it to test.py

--- pygama/dsp/__transforms__/pole_zero.py
+++ pygama/dsp/__transforms__/pole_zero.py
@@ -9,8 +9,10 @@ def pole_zero(wf_in, tau, wf_out):
     """
     Pole-zero correction using time constant tau
     """
-    np.copy_to(wf_out, wf_in)
+    # Do the following all in place within wf_in and wf_out:
+    # wf_out[i] = wf_out[i-1] + wf_in[i] - e^-1/tau*wf_in[i-1]
+    const = np.exp(-1/tau)
+    np.copyto(wf_out, wf_in)
     np.multiply(wf_out, const, wf_out)
-    np.subtract(wf_in[1::-1], wf_out[:-1:-1], wf_out[1::-1])
-    np.copy_to(wf_out[0], wf_in[0])
-    np.cumsum(wf_out, out=wf_out, axis=1)
+    np.subtract(wf_in[-1:1:-1], wf_out[-2:0:-1], wf_out[-1:1:-1])
+    np.cumsum(wf_out, out=wf_out, axis=0)
--- pygama/dsp/test.py
+++ pygama/dsp/test.py
@@ -1,9 +1,10 @@
 from ProcessingChain import ProcessingChain
-from transforms import mean_stdev, trap_filter
+from transforms import *
 import numpy as np
 from units import *
 from pygama.io import io_base as io
 
+verbose = 3 # 0=silent, 1=basic warnings, 2=basic output, 3=TMI
 nReps = 8 # number of times to loop over file
 wflen = 8192 # length of wf
 nblock = 8 # number of wfs to process at once
@@ -17,22 +18,21 @@ wf_in = data['waveform']['values'].nda
 dt = data['waveform']['dt'].nda[0] * unit_parser.parse_unit(data['waveform']['dt'].attrs['units'])
 
 # Set up processing chain
-proc = ProcessingChain(block_width=nblock, clock_unit=dt, verbosity=2)
+proc = ProcessingChain(block_width=nblock, clock_unit=dt, verbosity=verbose)
 proc.add_input_buffer("wf", wf_in, dtype='float32')
+
 proc.add_processor(mean_stdev, "wf[0:1000]", "bl", "bl_sig")
-bl=proc.get_output_buffer("bl")
-bl_sig=proc.get_output_buffer("bl_sig")
 proc.add_processor(np.subtract, "wf", "bl", "wf_blsub")
-proc.add_processor(trap_filter, "wf_blsub", 10*us, 5*us, "wf_trap")
+proc.add_processor(pole_zero, "wf_blsub", 70*us, "wf_pz")
+proc.add_processor(trap_filter, "wf_pz", 10*us, 5*us, "wf_trap")
 proc.add_processor(np.amax, "wf_trap", 1, "trapmax", signature='(n),()->()', types=['fi->f'])
 proc.add_processor(np.divide, "trapmax", 10*us, "trapE")
-Eout=proc.get_output_buffer("trapE")
 
 # Set up the LH5 output
 lh5_out = io.LH5Table(size=proc.__buffer_len__)
-lh5_out.add_field("trapE", io.LH5Array(Eout, attrs={"units":"ADC"}))
-lh5_out.add_field("bl", io.LH5Array(bl, attrs={"units":"ADC"}))
-lh5_out.add_field("bl_sig", io.LH5Array(bl_sig, attrs={"units":"ADC"}))
+lh5_out.add_field("trapE", io.LH5Array(proc.get_output_buffer("trapE"), attrs={"units":"ADC"}))
+lh5_out.add_field("bl", io.LH5Array(proc.get_output_buffer("bl"), attrs={"units":"ADC"}))
+lh5_out.add_field("bl_sig", io.LH5Array(proc.get_output_buffer("bl_sig"), attrs={"units":"ADC"}))
 
 print(proc)
 
