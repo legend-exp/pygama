8b7acb44309af5ee473975e24c14c58be7895c76
timmeh08 <timmeh08@uw.edu>
2020-03-12 14:44:17 -0700

    Asym trap

--- pygama/dsp/_processors/trap_filter.py
+++ pygama/dsp/_processors/trap_filter.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import guvectorize
+import matplotlib.pyplot as plt
 
 
 @guvectorize(["void(float32[:], int32, int32, float32[:])",
@@ -15,6 +16,12 @@ def trap_filter(wf_in, rise, flat, wf_out):
     wf_out[rise:] -= wf_in[:-rise]
     wf_out[rise+flat:] -= wf_in[:-(rise+flat)]
     wf_out[2*rise+flat:] += wf_in[:-(2*rise+flat)]
-    np.cumsum(wf_out, out=wf_out, axis=0)
+    wf_out = np.cumsum(wf_out, out=wf_out, axis=0)
+    x = np.arange(0,3000)
+    plt.plot(x, wf_in)
+    plt.plot(x, wf_out)
+    plt.show()
+    exit()
+
 
     #Plot every line
