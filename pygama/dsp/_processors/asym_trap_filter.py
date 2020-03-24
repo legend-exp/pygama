507ea1bf375b864d5170ef309768dfc6e512bbaa
timmeh08 <timmeh08@uw.edu>
2020-03-24 14:13:56 -0700

    more updates

--- pygama/dsp/_processors/asym_trap_filter.py
+++ pygama/dsp/_processors/asym_trap_filter.py
@@ -10,13 +10,31 @@ import matplotlib.pyplot as plt
 def asymTrapFilter(wf_in, rise, flat, fall, wf_out): #,padAfter=False
     """ Computes an asymmetric trapezoidal filter"""
 
-    wf_out.fill(0)
-    kernel = np.zeros(rise + flat + fall)
-    kernel[:rise] = 1 / rise
-    kernel[rise + flat:] = -1 / fall
+    # wf_out.fill(0)
+    # wf_out = np.zeros_like(wf_in)
+    kernel = np.zeros(2500)
+    kernel[:1000] = 1. / 1000
+    kernel[1500:] = 1. / 1000
 
     wf_out = np.convolve(wf_in, kernel, 'same')
-    
+
+
+    # wf_out[:] = wf_in[:]
+    # wf_out[rise:] -= wf_in[:-fall]
+    # wf_out[:] *= float(fall)/rise
+    # wf_out[rise+flat:] -= wf_in[:-(fall+flat)]
+    # wf_out[rise+flat+fall:] += wf_in[:-(rise+flat+fall)]
+    # wf_out[:] /= fall
+    # np.cumsum(wf_out, out=wf_out, axis=0)
+
+    # x = np.arange(0,3000)
+    # plt.scatter(x, wf_in)
+    # plt.scatter(x, wf_out)
+    # plt.show()
+    # exit()
+
+
+
     # npad = int((rise+fall+flat)/2)
     # wf_out = np.pad(wf_out, (npad, 0), mode='constant')[:-npad]
 
