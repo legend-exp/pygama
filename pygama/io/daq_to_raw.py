6c7a4fd89348ff0a93db35fe25d30794f4793d94
jasondet <jasondet@gmail.com>
2020-03-26 01:59:41 -0700

    handle run first.

--- pygama/io/daq_to_raw.py
+++ pygama/io/daq_to_raw.py
@@ -46,8 +46,8 @@ def daq_to_raw(daq_filename, raw_filename=None, run=None, prefix="t1",
             class SafeDict(dict):
                 def __missing__(self, key):
                     return '{' + key + '}'
+            if run is not None: raw_filename = raw_filename.format_map(SafeDict({ 'run': int(run) }))
             raw_filename = template.format_map(SafeDict(d2r_conf['filename_info']))
-            if run is not None: raw_filename = raw_filename.format_map(SafeDict({ 'run': run }))
         elif run is not None: raw_filename = f"{prefix}_run{run}.{suffix}"
         else:
             print('daq_to_raw error: must supply either raw_filename or run number')
