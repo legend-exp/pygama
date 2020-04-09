9ab7eab79b4e85ec334d6519003e18f07a18a6b4
Jason Detwiler <jasondet@gmail.com>
2020-04-08 23:06:20 -0700

    Be careful with raw_filename.

--- pygama/io/daq_to_raw.py
+++ pygama/io/daq_to_raw.py
@@ -42,12 +42,12 @@ def daq_to_raw(daq_filename, raw_filename=None, run=None, prefix="t1",
 
     if raw_filename is None:
         if 'raw_filename_template' in d2r_conf:
-            template = d2r_conf['raw_filename_template']
+            raw_filename = d2r_conf['raw_filename_template']
             class SafeDict(dict):
                 def __missing__(self, key):
                     return '{' + key + '}'
             if run is not None: raw_filename = raw_filename.format_map(SafeDict({ 'run': int(run) }))
-            raw_filename = template.format_map(SafeDict(d2r_conf['filename_info']))
+            raw_filename = raw_filename.format_map(SafeDict(d2r_conf['filename_info']))
         elif run is not None: raw_filename = f"{prefix}_run{run}.{suffix}"
         else:
             print('daq_to_raw error: must supply either raw_filename or run number')
