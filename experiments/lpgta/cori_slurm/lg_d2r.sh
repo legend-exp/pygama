a11026ab4254c35403e6a4252154086678b5560a
Jason Detwiler <jasondet@gmail.com>
2020-08-18 07:50:01 -0700

    add --date option

--- experiments/lpgta/cori_slurm/lg_d2r.sh
+++ experiments/lpgta/cori_slurm/lg_d2r.sh
@@ -9,9 +9,13 @@
 #SBATCH --output=/global/u1/l/lgprod/pygama/experiments/lpgta/cori_slurm/logs/cori-%j.txt
 
 echo "processing run $1"
-cd 
+cd /global/u1/l/lgprod/pygama/experiments/lpgta
 #shifter --image=docker:legendexp/legend-base:latest python processing.py --dg --q "run == $1" --d2r -o -v
-shifter python processing.py --dg --q "run == $1" --d2r -o -v
+#shifter python processing.py --dg --q "run == $1" --d2r -o -v
+#echo shifter python processing.py --dg --q \"YYYYmmdd == \'$1\'\" --d2r -o -v
+#shifter python processing.py --dg --q \"YYYYmmdd == $1\" --d2r -o -v
+echo shifter python processing.py --dg --date $1 --d2r -o -v
+shifter python processing.py --dg --date $1 --d2r -o -v
 
 #salloc -N 1 -C haswell -q interactive -t 04:00:00
-scontrol show job $JOBID
+#scontrol show job $JobID
