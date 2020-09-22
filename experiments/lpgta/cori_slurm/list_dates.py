67b4c37c25ed7ba874343b94c8728014115f4a44
Jason Detwiler <jasondet@gmail.com>
2020-09-22 12:59:47 -0700

    Unify job sub script and use "processing groups"  (group files by size for processing)

--- experiments/lpgta/cori_slurm/list_dates.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_hdf('../LPGTA_fileDB.h5')
-
-dates = df.groupby('YYYYmmdd').groups.keys()
-
-for date in dates: print(date)
--- experiments/lpgta/datagroup_setup.py
+++ experiments/lpgta/datagroup_setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os.path
 import pandas as pd
 import numpy as np
 from pygama import DataGroup
@@ -36,9 +37,33 @@ def main():
     dg.file_keys['runtype'] = dg.file_keys['rtp']
     
     dg.get_lh5_cols()
+
+    # append a column of filesizes
+    sizes_GB = []
+    proc_groups = []
+    proc_thresh_GB = 256
+    proc_LL_GB = 4
+    size_sum = 0
+    proc_group = 0
+    df = dg.file_keys
+    for daq_path, daq_file in zip(df['daq_dir'], df['daq_file']):
+        filename = os.path.expandvars('$LPGTA_DATA/daq')+daq_path+'/'+daq_file
+        sizes_GB.append(os.path.getsize(filename) / (1024**3))
+        # if this file pushes over the threshold, put in a new group
+        if size_sum + sizes_GB[-1] > proc_thresh_GB and size_sum > proc_LL_GB: 
+            proc_group += 1
+            size_sum = 0
+        proc_groups.append(proc_group)
+        size_sum += sizes_GB[-1]
+        if size_sum > proc_thresh_GB:
+            proc_group += 1
+            size_sum = 0
+
+    df['daq_size_GB'] = sizes_GB
+    df['proc_group'] = proc_groups
     
     # save to file used by processing.py
-    dg.save_df('./LPGTA_fileDB.h5')
+    dg.save_df('LPGTA_fileDB.h5')
     
     print(dg.file_keys)
         
