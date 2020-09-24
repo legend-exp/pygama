cc7d7dc46537a1a194e7aee5686e2668080adebf
Jason Detwiler <jasondet@gmail.com>
2020-09-24 09:30:37 -0700

    fix typos in comments

--- experiments/lpgta/cori_slurm/lg_proc.sh
+++ experiments/lpgta/cori_slurm/lg_proc.sh
@@ -13,12 +13,12 @@ date
 scontrol show job $SLURM_JOB_ID
 
 # To list proc groups and their sizes, do 
-# pdopen ../LPGTA_fileDB.h5
+# pdopenh5 ../LPGTA_fileDB.h5
 # and then in python
 # df.groupby(['proc_group'])['daq_size_GB'].sum()
 # 
 # To run all jobs do e.g.
-# for i in {0..23}; do sbatch lg_proc.sh d2r $i; done
+# for i in {0..49}; do sbatch lg_proc.sh d2r $i; done
 echo "-----------------------------------------------------------"
 echo "processing $1 for group $2"
 echo shifter python processing.py --dg --q "proc_group == $2" --$1 -o -v
