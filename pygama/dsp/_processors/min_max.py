e4a4a2772ca3f5e4f7c69054a98596cba16b95e3
George Marshall <gmarshall@pc204.hep.ucl.ac.uk>
2021-02-12 17:50:01 +0000

    changed min max to use inf

--- pygama/dsp/_processors/min_max.py
+++ pygama/dsp/_processors/min_max.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numba import guvectorize
-
+import math
 
 @guvectorize(["void(float32[:], int32[:], int32[:],float32[:], float32[:])",
               "void(float64[:], int64[:], int64[:],float64[:], float64[:] )"],
@@ -14,8 +14,14 @@ def min_max(wf, argmin, argmax, wf_min, wf_max, ):
     '''
 
     
-    wf_min[0] = wf[0]
-    wf_max[0] = wf[0]
+def min_max(wf, argmin, argmax, wf_min, wf_max, ):
+    '''
+    Finds the min, max and their time position for a waveform
+    '''
+
+
+    wf_min[0] = float(math.inf)
+    wf_max[0] = float(-math.inf)
     argmin[0] = 0
     argmax[0] = 0
 
@@ -23,6 +29,6 @@ def min_max(wf, argmin, argmax, wf_min, wf_max, ):
         if value < wf_min[0]:
             wf_min[0] = value
             argmin[0] = i
-        elif value > wf_max[0]:
+        if value > wf_max[0]:
             wf_max[0] = value
             argmax[0] = i
