a5229395321e3742aca73b9e58c5b4986ec15d32
Jason Detwiler <jasondet@uw.edu>
2021-02-23 13:52:10 -0800

    add parse_datatypes test

--- pygama/lgdo/test/test_lgdo_utils.py
+++ pygama/lgdo/test/test_lgdo_utils.py
@@ -19,3 +19,18 @@ def test_get_element_type():
         get_name = lgdo_utils.get_element_type(obj) 
         assert_(get_name == name, f'error with {name}: got {get_name}')
 
+datatypes = [ 
+              ( 'real', ('scalar', None, 'real') ),
+              ( 'array<1>{bool}', ('array', (1,), 'bool') ),
+              ( 'fixedsizearray<2>{real}', ('fixedsizearray', (2,), 'real') ),
+              ( 'arrayofequalsizedarrays<3,4>{complex}', ('arrayofequalsizedarrays', (3,4), 'complex') ),
+              ( 'array<1>{array<1>{blob}}', ('array', (1,), 'array<1>{blob}') ),
+              ( 'struct{field1,field2,fieldn}', ('struct', None, ['field1', 'field2', 'fieldn']) ),
+              ( 'table{col1,col2,coln}', ('table', None, ['col1', 'col2', 'coln']) ),
+            ]
+
+def test_parse_datatype():
+    for string, dt_tuple in datatypes:
+        pd_dt_tuple = lgdo_utils.parse_datatype(string)
+        assert_(pd_dt_tuple == dt_tuple, f'error with {dt_tuple[0]}: got {pd_dt_tuple}')
+
