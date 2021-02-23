6b5960bf2d35275681b7d5aa03f2f2d4ee7da1d7
Jason Detwiler <jasondet@uw.edu>
2021-02-23 12:18:50 -0800

    add test of get_element_type()

--- pygama/lgdo/test/test_lgdo_utils.py
+++ pygama/lgdo/test/test_lgdo_utils.py
@@ -1,1 +1,21 @@
-#placeholder
+import pytest
+import pygama.lgdo.lgdo_utils as lgdo_utils
+import numpy as np
+from numpy.testing import assert_
+
+objs = [ ('hi', 'string'),
+         (True, 'bool'),
+         (np.void(0), 'blob'),
+         (np.int(0), 'real'),
+         (np.uint8(0), 'real'),
+         (np.float(0), 'real'),
+         (1+1j, 'complex'),
+         (b'hi', 'string'),
+         (np.array(['hi']), 'string'),
+       ]
+
+def test_get_element_type():
+    for obj, name in objs:
+        get_name = lgdo_utils.get_element_type(obj) 
+        assert_(get_name == name, f'error with {name}: got {get_name}')
+
