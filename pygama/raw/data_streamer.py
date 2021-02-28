d479c4e6780b5ca4f7be54e0775ff1b91d31bb39
Jason Detwiler <jasondet@uw.edu>
2021-02-28 13:15:34 -0800

    first implementation

--- pygama/raw/data_streamer.py
+++ pygama/raw/data_streamer.py
@@ -1,1 +1,19 @@
-#placeholder
+from abc import ABC
+
+class DataStreamer(ABC):
+
+    def initialize(self, data_source, ch_groups_library=None, buffer_size=8192):
+        """
+        Returns
+        -------
+        header_data : list of lgdos
+        """
+        pass
+
+    def read_chunk(self):
+        """
+        Returns
+        -------
+        chunk : list of lgdos
+        """
+        pass
