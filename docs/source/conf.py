1d8e353825eb3e14914909a960ced5f65dbea8ae
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2021-03-09 16:47:22 +0100

    [ci] [docs] Do not run sphinx-apidoc, restore previous Napoleon settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,19 @@ extensions = [
     'sphinx_rtd_theme',
 ]
 
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
