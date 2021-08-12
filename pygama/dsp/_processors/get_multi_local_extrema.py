edea976667c89572a65f990a5845c414598330b8
SamuelBorden <sjborden@uw.edu>
2021-08-12 15:25:27 -0700

    Fixed checks and initalizations in multi_t

--- pygama/dsp/_processors/get_multi_local_extrema.py
+++ pygama/dsp/_processors/get_multi_local_extrema.py
@@ -1,15 +1,13 @@
 import numpy as np
-from numba import guvectorize, njit
+from numba import guvectorize
 from pygama.dsp.errors import DSPFatal
 
 
 
-@guvectorize(["void(float32[:], float32, float32[:], float32[:], float32[:], float32[:], float32[:], int32[:])",
-              "void(float64[:], float64, float64[:], float64[:], float64[:],float64[:], float64[:], int32[:])",
-              "void(int32[:], int32, int32[:], int32[:], int32[:], int32[:], int32[:], int32[:])",
-              "void(int64[:], int64, int64[:], int64[:], int64[:], int64[:], int64[:], int32[:])"],
+@guvectorize(["void(float32[:], float32, float32[:], float32[:], float32[:], float32[:], float32[:], float32[:])",
+              "void(float64[:], float64, float64[:], float64[:], float64[:],float64[:], float64[:], float32[:])"],
              "(n),(),(m) -> (m),(m),(),(),()", nopython=True, cache=True)
-def get_multi_local_extrema(w_in, delta_in, t_arbitrary_in, vt_max_out,vt_min_out,n_max_out,n_min_out,flag_out):
+def get_multi_local_extrema(w_in, delta_in, t_arbitrary_in, vt_max_out, vt_min_out, n_max_out, n_min_out, flag_out):
     """
     Get lists of indices of the local maxima and minima of data
     The "local" extrema are those maxima / minima that have heights / depths of
@@ -18,10 +16,12 @@ def get_multi_local_extrema(w_in, delta_in, t_arbitrary_in, vt_max_out,vt_min_ou
     Parameters
     ----------
     w_in : array-like
-        the array of data within which extrema will be found
+        The array of data within which extrema will be found
     delta_in : scalar
-        the absolute level by which data must vary (in one direction) about an
+        The absolute level by which data must vary (in one direction) about an
         extremum in order for it to be tagged
+    t_arbitrary_in : array-like
+        An array of fixed length that tells numba to expect to return a list of amplitudes of the same length
     Returns
     -------
     vt_max_out, vt_min_out : array-like, array-like
@@ -31,31 +31,39 @@ def get_multi_local_extrema(w_in, delta_in, t_arbitrary_in, vt_max_out,vt_min_ou
         The number of maxima and minima found in a waveform
     flag_out: scalar
         Returns 0 if there is only one maximum and it is a simple waveform, 
-        returns 1 if there are multiple peaks in a waveform
+        Returns 1 if there are multiple peaks in a waveform
         
     """
 
     # prepare output 
-    #padded with nans or zeros depending on what we want
+    
     vt_max_out[:]= np.nan
-    vt_min_out[:] = np.nan
-
+    vt_min_out[:] = np.nan    
+    n_max_out[0] = np.nan
+    n_min_out[0] = np.nan
+    flag_out[0] = np.nan
     
-    n_max_out[0] = 0
-    n_min_out[0] = 0
-    flag_out[0] = 0
+    # initialize internal counters
     
+    n_max_counter = 0 
+    n_min_counter = 0 
+    
+    
+    # Checks 
+    
+    if (np.isnan(w_in).any() or np.isnan(delta_in)):
+        return
 
-    # sanity checks
-    w_in, delta2 = np.asarray(w_in), np.asarray(delta_in)
-    if delta2.ndim != 0:
-        raise DSPFatal("Input argument delta_in must be a scalar")
-        
-    if delta_in <= 0:
-        raise DSPFatal("Input argument delta_in must be positive")
-        
+    if (not len(t_arbitrary_in)<len(w_in)):
+        raise DSPFatal('The length of your return array must be smaller than the length of your waveform')
+    if (not delta_in >= 0): 
+        raise DSPFatal('Delta must be positive')
+    if (not len(t_arbitrary_in)==len(vt_max_out) or not len(t_arbitrary_in)==len(vt_min_out)):
+        raise DSPFatal('Output arrays must be the same length as the arbitary input array')
+            
 
     # now loop over data
+    
     imax, imin = 0, 0
     find_max = True
     for i in range(len(w_in)):
@@ -66,26 +74,25 @@ def get_multi_local_extrema(w_in, delta_in, t_arbitrary_in, vt_max_out,vt_min_ou
         if find_max:
             # if the sample is less than the current max by more than delta,
             # declare the previous one a maximum, then set this as the new "min"
-            if w_in[i] < w_in[imax] - delta_in and int(n_max_out[0]) < int(len(vt_max_out)):
-                vt_max_out[int(n_max_out[0])]=imax
-                n_max_out[0] += 1
+            if w_in[i] < w_in[imax] - delta_in and int(n_max_counter) < int(len(vt_max_out)):
+                vt_max_out[int(n_max_counter)]=imax
+                n_max_counter += 1
                 imin = i
                 find_max = False
         else:
             # if the sample is more than the current min by more than delta,
             # declare the previous one a minimum, then set this as the new "max"
-            if w_in[i] > w_in[imin] + delta_in and int(n_min_out[0]) < int(len(vt_min_out)):
-                vt_min_out[int(n_min_out[0])] = imin
-                n_min_out[0] += 1
+            if w_in[i] > w_in[imin] + delta_in and int(n_min_counter) < int(len(vt_min_out)):
+                vt_min_out[int(n_min_counter)] = imin
+                n_min_counter += 1
                 imax = i
                 find_max = True
+                
+    # set output
+    n_max_out[0] = n_max_counter
+    n_min_out[0] = n_min_counter
+    
     if n_max_out[0] != 0:
         flag_out[0] = 1
-
-
-
-
-
-
-    
-
+    else:
+        flag_out[0] = 0
--- pygama/dsp/_processors/multi_t_filter.py
+++ pygama/dsp/_processors/multi_t_filter.py
@@ -1,16 +1,13 @@
 import numpy as np
-from numba import guvectorize, njit
+from numba import guvectorize
 from time_point_thresh import time_point_thresh
 from get_multi_local_extrema import get_multi_local_extrema
 
-
     
-@guvectorize(["void(float32[:],float32[:])",
-              "void(float64[:],float64[:])",
-              "void(int32[:],int32[:])",
-              "void(int64[:],int64[:])"],
-             "(m),(m)", nopython=True, cache=True)
-def remove_duplicates(t_out,vt_min_out):
+@guvectorize(["void(float32[:],float32[:],float32[:])",
+              "void(float64[:],float64[:],float64[:])"],
+             "(m),(m) -> (m)", nopython=True, cache=True)
+def remove_duplicates(t_in,vt_min_in, t_out):
     """ 
     time_point_thresh has issues with afterpulsing in waveforms that causes  
     an aferpulse peak's tp0 to be sent to 0 or the same index as the tp0 for the first pulse.
@@ -18,50 +15,64 @@ def remove_duplicates(t_out,vt_min_out):
     the afterpulse is greater than the threshold. So, we sweep through the array again 
     to ensure there are no duplicate indices. If there are duplicate indicies caused by a
     misidentified tp0 of an afterpulse, we replace its index by that of the corresponding minimum
-    found using the njit_get_i_local_extrema function.
+    found using the get_multi_local_extrema function. It also checks to make sure that the maximum of a waveform
+    isn't right at index 0.
     ----------
-    t_out : array-like
-        the array of indices that we want to remove duplicates from 
-    vt_min_out : array-like
-        list of indicies of minima that we want to replace duplicates in t_out with
+    t_in : array-like
+        The array of indices that we want to remove duplicates from 
+    vt_min_in : array-like
+        List of indicies of minima that we want to replace duplicates in t_out with
+    t_out: array-like
+        The array we want to return that will have no duplicate indices in it
     """
-
+    # initialize arrays
+    t_out[:] = np.nan
+    
+    # checks
+    if (np.isnan(t_in).all() and np.isnan(vt_min_in).all()): # we pad these with NaNs, so only return if there is nothing to analyze
+        return
+    
+    # check if any later indexed values are equal to the earliest instance
     k=0
-    for index1 in range(len(t_out)):
-        for index2 in range(len(t_out[index1+1:])):
-            if t_out[index1] == t_out[index2+index1+1]: # check if any later indexed values are equal to the earliest instance
-                t_out[index2+index1+1] = vt_min_out[k]
+    for index1 in range(len(t_in)):
+        for index2 in range(len(t_in[index1+1:])):
+            if t_in[index1] == t_in[index2+index1+1]: 
+                t_out[index2+index1+1] = vt_min_in[k]
         k+=1 # this makes sure that the index of the misidentified afterpulse tp0 is replaced with the correct corresponding minimum
-    if not np.isnan(t_out[0]): # makes sure that the first maximum found isn't the start of the waveform
+        
+    # Fill up the output with the rest of the values from the input that weren't repeats    
+    for index in range(len(t_in)): 
+        if (np.isnan(t_out[index]) and not np.isnan(t_in[index])):
+            t_out[index] = t_in[index]
+            
+    # makes sure that the first maximum found isn't the start of the waveform
+    if not np.isnan(t_out[0]):
         if int(t_out[0]) == 0: 
             t_out[:] = np.append(t_out[1:],np.nan)
 
 
 @guvectorize(["void(float32[:], float32[:],float32[:], float32, float32[:])",
-              "void(float64[:], float64[:], float64[:], float64, float64[:])",
-              "void(int32[:], int32[:], int32[:], int32, int32[:])",
-              "void(int64[:], int64[:], int64[:], int64, int64[:])"],
+              "void(float64[:], float64[:], float64[:], float64, float64[:])"],
              "(n),(m),(),()->(m)", forceobj=True, cache=True)
 def multi_t_filter(w_in, t_arbitrary_in, a_threshold_in, delta_in, t_out):
     """
     Gets list of indices of the start of leading edges of multiple peaks within a waveform.
     Is built to handle afterpulses/delayed cross talk and trains of pulses.
     The multi_t_filter works by calling the vectorized functions 
-    "get_i_local_extrema" which returns a list of the maxima and minima in a waveform,
+    "get_multi_local_extrema" which returns a list of the maxima and minima in a waveform,
     and then the list of maxima is fed into "time_point_thresh" which returns 
     the final times that waveform is less than a specified threshold. 
-
     Parameters
     ----------
     w_in : array-like
-        the array of data within which the list of tp0s will be found
+        The array of data within which the list of tp0s will be found
     t_arbitraty_in : array-like
         A fixed length array of the same dimensions that you need the tp0 list to be
         Used to tell numba what dimension of an array it should expect
     a_threshold_in: scalar 
-        threshold to search for using time_point_thresh
+        Threshold to search for using time_point_thresh
     delta_in : scalar
-        the absolute level by which data must vary (in one direction) about an
+        The absolute level by which data must vary (in one direction) about an
         extremum in order for it to be tagged
     Returns
     -------
@@ -72,22 +83,34 @@ def multi_t_filter(w_in, t_arbitrary_in, a_threshold_in, delta_in, t_out):
     """
      
     # initialize arrays, padded with the elements we want
-    
     t_out[:] = np.nan 
-    vt_max_out= np.zeros_like(t_arbitrary_in, dtype=np.float32)
-    vt_min_out = np.zeros_like(t_arbitrary_in,dtype=np.float32)
-    n_max_out = np.array([0],dtype=np.float32)
-    n_min_out = np.array([0],dtype=np.float32)
-    flag_out = np.array([0],dtype=np.float32)
-
-    # call get_i_local_extrema to get locations of the maxima and minima within a waveform
+    vt_max_out= np.full_like(t_arbitrary_in, np.nan, dtype=np.float32)
+    vt_min_out = np.full_like(t_arbitrary_in, np.nan, dtype=np.float32)
+    n_max_out = np.array([np.nan],dtype=np.float32)
+    n_min_out = np.array([np.nan],dtype=np.float32)
+    flag_out = np.array([np.nan],dtype=np.float32)
     
+    # checks 
+    if (np.isnan(w_in).any() or np.isnan(delta_in) or np.isnan(a_threshold_in)):
+        return
+    if (not len(t_arbitrary_in)<len(w_in)):
+        raise DSPFatal('The length of your return array must be smaller than the length of your waveform')
+    if (not delta_in >= 0): 
+        raise DSPFatal('Delta must be positive')
+    if (not len(t_arbitrary_in)==len(t_out)):
+        raise DSPFatal('Output arrays must be the same length as the arbitary input array')
+
+    # call get_multi_local_extrema to get locations of the maxima and minima within a waveform
     get_multi_local_extrema(w_in, delta_in,t_arbitrary_in, vt_max_out,vt_min_out,n_max_out,n_min_out,flag_out)
 
-
-    # Go through the list of maxima, calling time_point_thresh (the refactored version ignores the nan padding)
+    # set the walk_forward parameter so that we walk back using the refactored time_point_thresh
     walk_forward = np.array([0],dtype=np.int32)
-    time_point_thresh(w_in, a_threshold_in, vt_max_out, walk_forward, t_out)
+    
+    # Initialize an intermediate array to hold the tp0 values before we remove duplicates from it
+    intermediate_t_out = np.full_like(t_arbitrary_in, np.nan, dtype=np.float32)
+    
+    # Go through the list of maxima, calling time_point_thresh (the refactored version ignores the nan padding)
+    time_point_thresh(w_in, a_threshold_in, vt_max_out, walk_forward, intermediate_t_out)
+    
     # Remove duplicates from the t_out list
-    remove_duplicates(t_out,vt_min_out)
-
+    remove_duplicates(intermediate_t_out, vt_min_out, t_out)
