7389d1bbe0c8a95dbb3bb39293179dd5469e381b
David Sweigart <16840066+sweigart@users.noreply.github.com>
2021-08-16 15:51:37 -0400

    Conformed new processors to the conventions in template.py.

--- pygama/dsp/_processors/optimize.py
+++ pygama/dsp/_processors/optimize.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import guvectorize
+from pygama.dsp.errors import DSPFatal
 from .pole_zero import pole_zero, double_pole_zero
 from iminuit import Minuit
 
@@ -10,10 +11,10 @@ class Model:
     errordef = Minuit.LEAST_SQUARES
 
     # the constructor
-    def __init__(self, func, wf_in, baseline, beg, end):
+    def __init__(self, func, w_in, baseline, beg, end):
         self.func = func
         self.x    = np.arange(beg, end)
-        self.y    = np.asarray(wf_in, dtype=np.float64) - baseline
+        self.y    = np.asarray(w_in, dtype=np.float64) - baseline
         self.beg  = beg
         self.end  = end
 
@@ -22,32 +23,30 @@ class Model:
         y_pz = self.func(self.y, *args)[self.beg:self.end]
         return np.abs(np.sum(self.x) * np.sum(y_pz) - len(self.x) * np.sum(self.x * y_pz))
 
-@guvectorize(["void(float32[:], uint16 , int32, int32, float32, float32[:])",
-              "void(float64[:], uint16 , int64, int64, float64, float64[:])",
-              "void(float32[:], float32, int32, int32, float32, float32[:])",
-              "void(float64[:], float64, int64, int64, float64, float64[:])"],
+@guvectorize(["void(float32[:], float32, float32, float32, float32, float32[:])",
+              "void(float64[:], float64, float64, float64, float64, float64[:])"],
              "(n),(),(),(),()->()", forceobj=True)
-def optimize_1pz(wf_in, baseline, beg, end, p0, val0):
+def optimize_1pz(w_in, a_baseline_in, t_beg_in, t_end_in, p0_in, val0_out):
     """
     Find the optimal, single pole-zero cancellation's parameter
     by minimizing the slope in the waveform's specified time range.
     
     Parameters
     ----------
-    wf_in   : array-like
-              The input waveform
-    baseline: int
-              The resting baseline
-    beg     : int
-              The lower bound's index for the time range over
-              which to optimize the pole-zero cancellation
-    end     : int
-              The upper bound's index for the time range over
-              which to optimize the pole-zero cancellation
-    p0      : int
-              The initial guess of the optimal time constant
-    val0    : int
-              The output value of the best-fit time constant
+    w_in         : array-like
+                   The input waveform
+    a_baseline_in: float
+                   The resting baseline
+    t_beg_in     : int
+                   The lower bound's index for the time range over
+                   which to optimize the pole-zero cancellation
+    t_end_in     : int
+                   The upper bound's index for the time range over
+                   which to optimize the pole-zero cancellation
+    p0_in        : float
+                   The initial guess of the optimal time constant
+    val0_out     : float
+                   The output value of the best-fit time constant
               
     Processing Chain Example
     ------------------------
@@ -59,47 +58,59 @@ def optimize_1pz(wf_in, baseline, beg, end, p0, val0):
         "unit": "us"
     }
     """
-    m = Minuit(Model(pole_zero, wf_in, baseline, beg, end), [p0])
+    val0_out[0] = np.nan
+
+    if np.isnan(w_in).any() or np.isnan(a_baseline_in) or np.isnan(t_beg_in) or np.isnan(t_end_in) or\
+       np.isnan(p0_in):
+        return
+
+    if not np.floor(t_beg_in) == t_beg_in or\
+       not np.floor(t_end_in) == t_end_in:
+        raise DSPFatal('The waveform index is not an integer')
+
+    if int(t_beg_in) < 0 or int(t_beg_in) > len(w_in) or\
+       int(t_end_in) < 0 or int(t_end_in) > len(w_in):
+        raise DSPFatal('The waveform index is out of range')
+
+    m = Minuit(Model(pole_zero, w_in, a_baseline_in, int(t_beg_in), int(t_end_in)), [p0_in])
     m.print_level = -1
     m.strategy = 1
     m.errordef = Minuit.LEAST_SQUARES
     m.migrad()
-    val0[0] = m.values[0]
+    val0_out[0] = m.values[0]
     
-@guvectorize(["void(float32[:], uint16 , int32, int32, float32, float32, float32, float32[:], float32[:], float32[:])",
-              "void(float64[:], uint16 , int64, int64, float64, float64, float64, float64[:], float64[:], float64[:])",
-              "void(float32[:], float32, int32, int32, float32, float32, float32, float32[:], float32[:], float32[:])",
-              "void(float64[:], float64, int64, int64, float64, float64, float64, float64[:], float64[:], float64[:])"],
+@guvectorize(["void(float32[:], float32, float32, float32, float32, float32, float32, float32[:], float32[:], float32[:])",
+              "void(float64[:], float64, float64, float64, float64, float64, float64, float64[:], float64[:], float64[:])"],
              "(n),(),(),(),(),(),()->(),(),()", forceobj=True)
-def optimize_2pz(wf_in, baseline, beg, end, p0, p1, p2, val0, val1, val2):
+def optimize_2pz(w_in, a_baseline_in, t_beg_in, t_end_in, p0_in, p1_in, p2_in, val0_out, val1_out, val2_out):
     """
     Find the optimal, double pole-zero cancellation's parameters
     by minimizing the slope in the waveform's specified time range.
     
     Parameters
     ----------
-    wf_in   : array-like
-              The input waveform
-    baseline: int
-              The resting baseline
-    beg     : int
-              The lower bound's index for the time range over
-              which to optimize the pole-zero cancellation
-    end     : int
-              The upper bound's index for the time range over
-              which to optimize the pole-zero cancellation
-    p0      : int
-              The initial guess of the optimal, longer time constant
-    p1      : int
-              The initial guess of the optimal, shorter time constant
-    p2      : int
-              The initial guess of the optimal fraction 
-    val0    : int
-              The output value of the best-fit, longer time constant
-    val1    : int
-              The output value of the best-fit, shorter time constant
-    val2    : int
-              The output value of the best-fit fraction
+    w_in         : array-like
+                   The input waveform
+    a_baseline_in: float
+                   The resting baseline
+    t_beg_in     : int
+                   The lower bound's index for the time range over
+                   which to optimize the pole-zero cancellation
+    t_end_in     : int
+                   The upper bound's index for the time range over
+                   which to optimize the pole-zero cancellation
+    p0_in        : float
+                   The initial guess of the optimal, longer time constant
+    p1_in        : float
+                   The initial guess of the optimal, shorter time constant
+    p2_in        : float
+                   The initial guess of the optimal fraction
+    val0_out     : float
+                   The output value of the best-fit, longer time constant
+    val1_out     : float
+                   The output value of the best-fit, shorter time constant
+    val2_out     : float
+                   The output value of the best-fit fraction
               
     Processing Chain Example
     ------------------------
@@ -111,11 +122,27 @@ def optimize_2pz(wf_in, baseline, beg, end, p0, p1, p2, val0, val1, val2):
         "unit": "us"
     }
     """
-    m = Minuit(Model(double_pole_zero, wf_in, baseline, beg, end), [p0, p1, p2])
+    val0_out[0] = np.nan
+    val1_out[0] = np.nan
+    val2_out[0] = np.nan
+
+    if np.isnan(w_in).any() or np.isnan(a_baseline_in) or np.isnan(t_beg_in) or np.isnan(t_end_in) or\
+       np.isnan(p0_in) or np.isnan(p1_in) or np.isnan(p2_in):
+        return
+
+    if not np.floor(t_beg_in) == t_beg_in or\
+       not np.floor(t_end_in) == t_end_in:
+        raise DSPFatal('The waveform index is not an integer')
+
+    if int(t_beg_in) < 0 or int(t_beg_in) > len(w_in) or\
+       int(t_end_in) < 0 or int(t_end_in) > len(w_in):
+        raise DSPFatal('The waveform index is out of range')
+
+    m = Minuit(Model(double_pole_zero, w_in, a_baseline_in, int(t_beg_in), int(t_end_in)), [p0_in, p1_in, p2_in])
     m.print_level = -1
     m.strategy = 1
     m.errordef = Minuit.LEAST_SQUARES
     m.migrad()
-    val0[0] = m.values[0]
-    val1[0] = m.values[1]
-    val2[0] = m.values[2]
+    val0_out[0] = m.values[0]
+    val1_out[0] = m.values[1]
+    val2_out[0] = m.values[2]
--- pygama/dsp/_processors/saturation.py
+++ pygama/dsp/_processors/saturation.py
@@ -1,10 +1,11 @@
 import numpy as np
 from numba import guvectorize
+from pygama.dsp.errors import DSPFatal
 
-@guvectorize(["void(float32[:], uint16, uint16[:], uint16[:])",
-              "void(float64[:], uint16, uint16[:], uint16[:])"],
+@guvectorize(["void(float32[:], float32, float32[:], float32[:])",
+              "void(float64[:], float64, float64[:], float64[:])"],
              "(n),()->(),()", nopython=True, cache=True)
-def saturation(wf_in, bit_depth, n_lo, n_hi):
+def saturation(w_in, bit_depth_in, n_lo_out, n_hi_out):
     """
     Count the number of samples in the waveform that are
     saturated at the minimum and maximum possible values based
@@ -12,14 +13,14 @@ def saturation(wf_in, bit_depth, n_lo, n_hi):
     
     Parameters
     ----------
-    wf_in    : array-like
-               The input waveform
-    bit_depth: int
-               The bit depth of the analog-to-digital converter
-    n_lo     : int
-               The output number of samples at the minimum
-    n_hi     : int
-               The output number of samples at the maximum
+    w_in        : array-like
+                  The input waveform
+    bit_depth_in: int
+                  The bit depth of the analog-to-digital converter
+    n_lo_out    : int
+                  The output number of samples at the minimum
+    n_hi_out    : int
+                  The output number of samples at the maximum
     
     Processing Chain Example
     ------------------------
@@ -31,10 +32,22 @@ def saturation(wf_in, bit_depth, n_lo, n_hi):
         "prereqs": ["waveform"]
     }
     """
-    n_lo[0] = 0
-    n_hi[0] = 0
-    for sample in wf_in:
-        if sample == 0:
-            n_lo[0] += 1
-        elif sample == np.power(2, bit_depth):
-            n_hi[0] += 1
+    n_lo_out[0] = np.nan
+    n_hi_out[0] = np.nan
+
+    if np.isnan(w_in).any() or np.isnan(bit_depth_in):
+        return
+
+    if not np.floor(bit_depth_in) == bit_depth_in:
+        raise DSPFatal('The bit depth is not an integer')
+
+    if bit_depth_in <= 0:
+        raise DSPFatal('The bit depth is not positive')
+
+    n_lo_out[0] = 0
+    n_hi_out[0] = 0
+    for i in range(0, len(w_in), 1):
+        if w_in[i] == 0:
+            n_lo_out[0] += 1
+        elif w_in[i] == np.power(2, int(bit_depth_in)):
+            n_hi_out[0] += 1
