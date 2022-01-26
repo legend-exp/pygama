3ab0d06ca0437878362f8b846233803301a259f4
Luigi Pertoldi <gipert@pm.me>
2022-01-26 12:06:02 +0100

    [docs] Use sphinx-multiversion to build docs for each branch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,11 +19,6 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'pygama'
 copyright = '2020, LEGEND Collaboration'
-author = 'C. Wiseman'
-
-# The full version, including alpha/beta/rc tags
-release = 'v0.6'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -39,6 +34,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
+    'sphinx_multiversion',
 ]
 
 # The suffix(es) of source filenames.
@@ -99,3 +95,23 @@ napoleon_use_rtype = True
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+# -- Options for sphinx-multiversion -----------------------------------------
+
+# Whitelist pattern for tags (set to None to ignore all tags)
+smv_tag_whitelist = None
+
+# Whitelist pattern for branches (set to None to ignore all branches)
+smv_branch_whitelist = r'^(master|dev|refactor)$'
+
+# Whitelist pattern for remotes (set to None to use local branches only)
+smv_remote_whitelist = None
+
+# Pattern for released versions
+smv_released_pattern = r'^tags/.*$'
+
+# Format for versioned output directories inside the build directory
+smv_outputdir_format = '{ref.name}'
+
+# Determines whether remote or local git branches/tags are preferred if their output dirs conflict
+smv_prefer_remote_refs = False
