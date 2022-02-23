1fee0d8f0e6e2cac685c5a5ec391837ab6f24215
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-02-23 14:08:50 +0100

    Improve version handling
    Now the version string is manually specified in pygama/version.py *only*
    and automatically parsed by setup.py.

--- pygama/__init__.py
+++ pygama/__init__.py
@@ -27,6 +27,6 @@ should ever by "blank" as specified by option 1: you should at least include a
 docstring explaining what the package does. This will help users poking around
 in ipython, etc.
 """
-__version__ = "0.1.0"
 
-from .analysis.datagroup import DataGroup
+from pygama.version import __version__
+from pygama.analysis.datagroup import DataGroup
