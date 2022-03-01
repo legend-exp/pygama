193c603d962817271821efbf69fefdebcccab95b
wisecg <wisecg.neontetra@gmail.com>
2022-03-01 12:41:13 -0800

    fix typo

--- pygama/raw/build_raw.py
+++ pygama/raw/build_raw.py
@@ -11,9 +11,9 @@ from fc.fc_streamer import FCStreamer
 #from stream_fc import *
 
 
-def build_raw_files(in_stream, in_stream_type, out_spec=None, buffer_size=8192, 
-                    n_max=np.inf, overwrite=True, verbosity=0)
-    """ Convert data into LEGEND hdf5 `raw` format.  
+def build_raw_files(in_stream, in_stream_type, out_spec=None, buffer_size=8192,
+                    n_max=np.inf, overwrite=True, verbosity=0):
+    """ Convert data into LEGEND hdf5 `raw` format.
 
     Takes an input stream (in_stream) of a given type (in_stream_type) and
     writes to output file(s) according to the user's a specification (out_spec).
@@ -28,12 +28,12 @@ def build_raw_files(in_stream, in_stream_type, out_spec=None, buffer_size=8192,
         Type of stream used to write the input file.
         Options are 'ORCA', 'FlashCams', 'LlamaDaq', 'Compass', 'MGDO'
     out_spec : str or json dict or RawBufferLibrary or None
-        Specification for the output stream. 
+        Specification for the output stream.
         - If a str, interpretted as a filename for
           an output hdf5 file, in which each stream decoder writes its own
           stream to an hdf5 group named for the decoder. Can use environment
           variables.
-        - If None, uses '{in_stream}.hdf5' as the output filename.  
+        - If None, uses '{in_stream}.hdf5' as the output filename.
         - If a RawBufferLibrary, the mapping of data to output file / group is
           taken from that.
         - If a json dict, should be a dict loaded from the json shorthand
@@ -51,12 +51,12 @@ def build_raw_files(in_stream, in_stream_type, out_spec=None, buffer_size=8192,
 
     # convert any environment variables in in_stream so that we can check for readability
     in_stream = os.path.expandvars(in_stream)
-    # later: fix if in_stream is not a file 
+    # later: fix if in_stream is not a file
     # (e.g. a socket, which exists if open and has size = np.inf)
     if not os.path.exists(in_stream):
         print(f'Error: file {in_stream} not found')
         return
-    in_stream_size = os.stat(in_stream).st_size 
+    in_stream_size = os.stat(in_stream).st_size
 
     # set up RawBufferLibrary
     if isinstance(out_spec, RawBufferLibrary): rb_lib = out_spec
@@ -76,13 +76,13 @@ def build_raw_files(in_stream, in_stream_type, out_spec=None, buffer_size=8192,
     # ouput start of processing info if verbosity > 0
     if verbosity > 0:
         output = out_spec
-        if isinstance(out_spec, RawBufferLibrary): 
+        if isinstance(out_spec, RawBufferLibrary):
             output = out_spec.get_list_of('out_stream')
         print( 'Starting build_raw processing.')
-        print(f'  Input: {in_stream}')        
+        print(f'  Input: {in_stream}')
         print(f'  Output: {pprint(output)}')
-        print(f'  Buffer size: {buffer_size}')  
-        print(f'  Max num. events: {n_max}')    
+        print(f'  Buffer size: {buffer_size}')
+        print(f'  Max num. events: {n_max}')
 
     # start a timer and a byte counter
     t_start = time.time()
@@ -109,7 +109,7 @@ def build_raw_files(in_stream, in_stream_type, out_spec=None, buffer_size=8192,
         return
 
     # initialize the stream and read header. Also initializes rb_lib
-    header_data, n_bytes = streamer.initialize(in_stream, in_stream_type, rb_lib, 
+    header_data, n_bytes = streamer.initialize(in_stream, in_stream_type, rb_lib,
                                                buffer_size=buffer_size, verbosity=verbosity)
     bytes_processed += n_bytes
     if verbosity > 0: update_progress(float(n_bytes)/in_stream_size)
