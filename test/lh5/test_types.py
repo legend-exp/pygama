8013f6008d345ad792a99225be4d4825d6422bd8
Jason Detwiler <jasondet@uw.edu>
2022-03-08 22:42:29 -0800

    get test_types.py to work on refactor branch

--- test/lh5/test_types.py
+++ test/lh5/test_types.py
@@ -1,6 +1,6 @@
-import pygama.lh5 as lh5
+import pygama.lgdo as lgdo
 
 
 def test_array():
-    a = lh5.Array(shape=(1), dtype=float)
+    a = lgdo.Array(shape=(1), dtype=float)
     assert a.dataype_name() == 'array'
