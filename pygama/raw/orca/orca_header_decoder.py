f4c779532d151bfc25b679aa87114b05fb75aecc
Jason Detwiler <jasondet@uw.edu>
2022-04-26 22:48:08 -0700

    initial header decoder refactor

---
+++ pygama/raw/orca/orca_header_decoder.py
@@ -0,0 +1,36 @@
+from ..data_decoder import *
+from pygama import lgdo
+import plistlib
+
+class OrcaHeaderDecoder(DataDecoder):
+    """
+    Decodes Orca headers
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.header = lgdo.Struct()
+
+    def make_lgdo(self, key=None, size=None): return self.header
+
+    def buffer_is_full(self, rb): return rb.loc > 0
+
+    def decode_header(self, packet):
+        n_bytes = packet[1] # word 1 is the header packet length in bytes
+        as_bytes = packet[2:].tobytes()
+        header_dict = plistlib.loads(as_bytes[:n_bytes], fmt=plistlib.FMT_XML)
+        self.header.update(header_dict)
+
+    def get_decoder_list(self):
+        decoder_names = []
+        dd = self.header['dataDescription']
+        for class_key in dd.keys():
+            for super_key in dd[class_key].keys():
+                decoder_names.append(dd[class_key][super_key]['decoder'])
+        return decoder_names
+
+    def get_data_id(self, decoder_name):
+        dd = header_dict['dataDescription']
+        for class_key in dd.keys():
+            for super_key in dd[class_key].keys():
+                if dd[class_key][super_key]['decoder'] == decoder_name:
+                    return dd[class_key][super_key]['dataId']
