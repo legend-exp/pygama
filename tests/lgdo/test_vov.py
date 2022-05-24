a729880b2034f540461e258a957a8c551e0fbe34
Grace <grsong@uw.edu>
2022-05-24 15:40:32 -0700

    fix test

--- tests/lgdo/test_vov.py
+++ tests/lgdo/test_vov.py
@@ -8,7 +8,8 @@ import pygama.lgdo as lgdo
 @pytest.fixture()
 def vov():
     print('--------setup--------')
-    yield lgdo.VectorOfVectors()
+
+    yield lgdo.VectorOfVectors
     print('--------tear down--------')
 
 @pytest.fixture()
@@ -16,21 +17,27 @@ def array():
     print('--------setup--------')
     yield lgdo.Array
 
-class Test_VectorOfVectors:
 
-    def test_datatype_name(self, vov):
-        result = vov.datatype_name()
+class Test_VectorOfVectors:
+    def test_datatype_name(self, vov, array):
+        flat = array(nda=np.array([1, 2, 3, 4, 5, 2, 4, 8, 9, 7, 5, 3, 1]))
+        length = array(nda=np.array([2, 5, 6, 10, 13]))
+        v = vov(flattened_data=flat, cumulative_length=length)
+        result = v.datatype_name()
         desired = 'array'
         assert_(result == desired)
 
 
-    def test_list(self, vov):
-        flat = array(np.array([1, 2, 3, 4, 5, 2, 4, 8, 9, 7, 5, 3, 1]))
-        length = array(np.array([2, 5, 6, 10, 13]))
+    def test_list(self, vov, array):
+        flat = array(nda=np.array([1, 2, 3, 4, 5, 2, 4, 8, 9, 7, 5, 3, 1]))
+        length = array(nda=np.array([2, 5, 6, 10, 13]))
         v = vov(flattened_data=flat, cumulative_length=length)
-        result = list(vov)
+        result = list(v)
 
-        desired = [np.array(v.flattened_data.nda[:v.cumulative_length.nda[0]])]
-        desired += [np.array(v.flattened_data.nda[v.cumulative_length.nda[i]:v.cumulative_length.nda[i+1]]) for i in range(len(v.cumulative_length))]
-
-        assert_(desired==result)
+        desired = [ np.array([1,2]),
+                    np.array([3,4,5]), 
+                    np.array([2]),
+                    np.array([4,8,9,7]),
+                    np.array([5, 3, 1]) ]
+                    
+        assert_( (desired[i]==result[i]).all() for i in range(len(desired)) )
