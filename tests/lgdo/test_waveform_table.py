67c5d2d673c53c43beaf753da18eee83ff9705e4
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-06-02 13:46:57 +0200

    [tests] [`lgdo`] Add more `WaveformTable` tests

--- tests/lgdo/test_waveform_table.py
+++ tests/lgdo/test_waveform_table.py
@@ -4,9 +4,8 @@ import pygama.lgdo as lgdo
 from pygama.lgdo.waveform_table import WaveformTable
 
 
-# TODO: add comparison operators for lgdo's?
 def test_init():
-    wft = WaveformTable()  # size = 1024 and wf_len = 100
+    wft = WaveformTable()  # defaults: size = 1024 and wf_len = 100
     assert (wft.t0.nda == np.zeros(1024)).all()
     assert (wft.dt.nda == np.full(1024, fill_value=1)).all()
     assert isinstance(wft.values, lgdo.VectorOfVectors)
@@ -36,11 +35,11 @@ def test_init():
     assert isinstance(wft.values, lgdo.ArrayOfEqualSizedArrays)
     assert (wft.values.nda == np.full(shape=(10, 1000), fill_value=69)).all()
 
-    # wft = WaveformTable(values=lgdo.VectorOfVectors(shape_guess=(10, 1000)))
-    # assert (wft.t0.nda == np.zeros(10)).all()
-    # assert (wft.dt.nda == np.full(10, fill_value=1)).all()
-    # assert isinstance(wft.values, lgdo.VectorOfVectors)
-    # assert (wft.values.nda == np.full(shape=(10, 1000), fill_value=69)).all()
+    wft = WaveformTable(values=lgdo.VectorOfVectors(shape_guess=(10, 1000), dtype=np.float32))
+    assert (wft.t0.nda == np.zeros(10)).all()
+    assert (wft.dt.nda == np.full(10, fill_value=1)).all()
+    assert isinstance(wft.values, lgdo.VectorOfVectors)
+    assert len(wft.values) == 10
 
     wft = WaveformTable(t0=[1, 1, 1], dt=[2, 2, 2], values=lgdo.ArrayOfEqualSizedArrays(shape=(3, 1000)))
     assert (wft.t0.nda == np.full(3, fill_value=1)).all()
