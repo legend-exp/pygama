7baf50923ff61c1dca881135b2f4d615ce41410a
Jason Detwiler <jasondet@uw.edu>
2022-06-02 00:27:03 -0700

    add a hex_dump in orca_packet

--- src/pygama/raw/orca/orca_packet.py
+++ src/pygama/raw/orca/orca_packet.py
@@ -1,8 +1,11 @@
 '''
-orca packets are ndarrays of type uint32
+orca_packet provides convenience functions for working with orca packets.
 
+orca packets are ndarrays of type uint32
 1 'word' is one uint32
 '''
+
+import numpy as np
 
 def is_short(packet):
     return bool(packet[0] >> 31)
@@ -17,3 +20,20 @@ def get_data_id(packet, shift=True):
         else: return packet[0] & 0xFC000000
     if shift: return (packet[0] & 0xFFFC0000) >> 18
     return packet[0] & 0xFFFC0000
+
+def hex_dump(packet, shift_data_id=True, print_n_words=False, max_words=np.inf,
+             as_int=False, as_short=False):
+    data_id = get_data_id(packet, shift=shift_data_id)
+    n_words = get_n_words(packet)
+    if print_n_words: print(f'data ID = {data_id}: {n_words} words')
+    else:
+        print(f'data ID = {data_id}:')
+        n_to_print = int(np.minimum(n_words, max_words))
+        pad = int(np.ceil(np.log10(n_to_print)))
+        for i in range(n_to_print):
+            line = f'{str(i).zfill(pad)}'
+            line += ' {0:#0{1}x}'.format(packet[i], 10)
+            if data_id == 0 and i > 1: line += f' {packet[i:i+1].tobytes().decode()}'
+            if as_int: line += f' {packet[i]}'
+            if as_short: line += f" {np.frombuffer(packet[i:i+1].tobytes(), dtype='uint16')}"
+            print(line)
