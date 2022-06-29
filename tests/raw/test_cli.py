34e7c3bdb212cb071bae3e81234c0917877d4c42
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-06-29 15:33:34 +0200

    [cli] bug fixes & tests
    Add `orig_basename` special variable that holds the original filename
    (without extension) in the JSON config. Available through CLI only.

---
+++ tests/raw/test_cli.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import subprocess
+
+config_dir = Path(__file__).parent/'configs'
+
+
+def test_build_raw_cli(lgnd_test_data):
+    subprocess.check_call(['pygama', 'build-raw', '--help'])
+    subprocess.check_call([
+        'pygama', 'build-raw',
+        '--overwrite',
+        '--stream-type', 'ORCA',
+        '--out-spec', f'{config_dir}/orca-out-spec.json',
+        '--max-rows', '10',
+        '--buffer_size', '8192',
+        lgnd_test_data.get_path('orca/fc/L200-comm-20220519-phy-geds.orca')
+    ])
