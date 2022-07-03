1dc080c0e27275cd0aa6d5a8b389664113a56ee7
Jason Detwiler <jasondet@uw.edu>
2022-07-03 00:53:07 -0700

    Make TODO not look like a parameter

--- src/pygama/math/functions/crystall_ball.py
+++ src/pygama/math/functions/crystall_ball.py
@@ -94,6 +94,12 @@ def nb_xtalball_cdf(x: float, beta: float, m: float, mu: float, sigma: float, A:
         The amount to scale the distribution
     A : float
         An overall scale factor
+
+    Returns
+    -------
+    y : float
+        The value at x given the parameters
+
     TODO: Potentially replace by a guvectorized factory function so that the normalization isn't computed every time
     """
     if (beta <= 0) or (m <= 1):
