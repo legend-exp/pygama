aeef22a85e2ff592ea8c4d0be37d9ff0c1ca7ad9
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-07-05 12:31:16 +0200

    [`lgdo`] Use type hints, logging module, improve docstrings

--- src/pygama/lgdo/array.py
+++ src/pygama/lgdo/array.py
@@ -1,46 +1,58 @@
+"""
+Implements a LEGEND Data Object representing an n-dimensional array and
+corresponding utilities.
+"""
+from __future__ import annotations
+
+from typing import Any
+import logging
 import numpy as np
 
-from .lgdo_utils import *
+from pygama.lgdo.lgdo_utils import get_element_type
+
+log = logging.getLogger(__name__)
 
 
 class Array:
-    """Holds an ndarray and attributes
+    r"""Holds an :class:`numpy.ndarray` and attributes.
 
-    Array (and the other various array types) holds an "nda" instead of deriving
-    from ndarray for the following reasons:
+    :class:`Array` (and the other various array types) holds an `nda` instead
+    of deriving from :class:`numpy.ndarray` for the following reasons:
 
-    - it keeps management of the nda totally under the control of the user. The
-      user can point it to another object's buffer, grab the nda and toss the
-      Array, etc.
-    - it allows the management code to send just the nda's the central routines
-      for data manpulation. Keeping lgdo's out of that code allows for more
-      standard, reusable, and (we expect) performant python
-    - it allows the first axis of the nda to be treated as "special" for storage
-      in Tables
+    - It keeps management of the `nda` totally under the control of the user. The
+      user can point it to another object's buffer, grab the `nda` and toss the
+      :class:`Array`, etc.
+    - It allows the management code to send just the `nda`'s the central routines
+      for data manpulation. Keeping LGDO's out of that code allows for more
+      standard, reusable, and (we expect) performant Python.
+    - It allows the first axis of the `nda` to be treated as "special" for storage
+      in :class:`.Table`\ s.
 
     """
 
-
-    def __init__(self, nda=None, shape=(), dtype=None, fill_val=None, attrs={}):
+    def __init__(self, nda: np.ndarray = None, shape: tuple[int, ...] = (),
+                 dtype: np.dtype = None, fill_val: float | int = None,
+                 attrs: dict[str, Any] = {}) -> None:
         """
         Parameters
         ----------
-        nda : ndarray (optional)
-            An ndarray to be used for this object's internal array. Note: the
-            array is used directly, not copied. If not supplied, internal memory
-            is newly allocated based on the shape and dtype arguments.
-        shape : tuple of ints (optional)
+        nda
+            An :class:`numpy.ndarray` to be used for this object's internal
+            array. Note: the array is used directly, not copied. If not
+            supplied, internal memory is newly allocated based on the shape and
+            dtype arguments.
+        shape
             A numpy-format shape specification for shape of the internal
-            ndarray. Required if nda is None, otherwise unused.
-        dtype : numpy dtype (optional)
-            Specifies the type of the data in the array. Required if nda is
-            None, otherwise unused.
-        fill_val : scalar or None
-            If None, memory is allocated without initialization. Otherwise, the
-            array is allocated with all elements set to the corresponding fill
-            value. If nda is not None, this parameter is ignored
-        attrs : dict (optional)
-            A set of user attributes to be carried along with this lgdo
+            ndarray. Required if `nda` is ``None``, otherwise unused.
+        dtype
+            Specifies the type of the data in the array. Required if `nda` is
+            ``None``, otherwise unused.
+        fill_val
+            If ``None``, memory is allocated without initialization. Otherwise,
+            the array is allocated with all elements set to the corresponding
+            fill value. If `nda` is not ``None``, this parameter is ignored.
+        attrs
+            A set of user attributes to be carried along with this LGDO.
         """
         if nda is None:
             if fill_val is None: nda = np.empty(shape, dtype=dtype)
@@ -51,42 +63,39 @@ class Array:
         self.attrs = dict(attrs)
         if 'datatype' in self.attrs:
             if self.attrs['datatype'] != self.form_datatype():
-                print(type(self).__name__ + ': Warning: datatype does not match nda!')
-                print('datatype: ', self.attrs['datatype'])
-                print('form_datatype(): ', self.form_datatype())
-                print('dtype:', self.dtype)
-        else: self.attrs['datatype'] = self.form_datatype()
+                log.warning(('datatype does not match nda! '
+                             f'datatype: {self.attrs["datatype"]} '
+                             f'form_datatype(): {self.form_datatype()} '
+                             f'dtype: {self.dtype}'))
+        else:
+            self.attrs['datatype'] = self.form_datatype()
 
-
-    def datatype_name(self):
-        """The name for this lgdo's datatype attribute"""
+    def datatype_name(self) -> str:
+        """The name for this LGDO's datatype attribute."""
         return 'array'
 
-
-    def form_datatype(self):
-        """Return this lgdo's datatype attribute string"""
+    def form_datatype(self) -> str:
+        """Return this LGDO's datatype attribute string."""
         dt = self.datatype_name()
         nD = str(len(self.nda.shape))
         et = get_element_type(self)
         return dt + '<' + nD + '>{' + et + '}'
 
-
-    def __len__(self):
-        """Provides __len__ for this array-like class"""
+    def __len__(self) -> int:
         return len(self.nda)
 
-
-    def resize(self, new_size):
-        """Resize the array to new_size (int)"""
+    def resize(self, new_size: int) -> None:
+        """Resize the array to `new_size`."""
         new_shape = (new_size,) + self.nda.shape[1:]
         self.nda.resize(new_shape, refcheck=True)
 
-    def __str__(self):
-        """Convert to string (e.g. for printing)"""
+    def __str__(self) -> str:
         tmp_attrs = self.attrs.copy()
         datatype = tmp_attrs.pop('datatype')
         string = datatype + " = " + str(self.nda)
-        if len(tmp_attrs) > 0: string += '\n attrs = ' + str(tmp_attrs)
+        if len(tmp_attrs) > 0:
+            string += '\n attrs = ' + str(tmp_attrs)
         return string
 
-    def __repr__(self): return str(self)
+    def __repr__(self) -> str:
+        return str(self)
--- src/pygama/lgdo/table.py
+++ src/pygama/lgdo/table.py
@@ -1,46 +1,63 @@
-import numpy as np
+"""
+Implements a LEGEND Data Object representing a special struct of arrays of
+equal length and corresponding utilities.
+"""
+
+from __future__ import annotations
+
+from typing import Any
+import logging
+
 import pandas as pd
 
-from .struct import Struct
-from .vectorofvectors import VectorOfVectors
+from pygama.lgdo.array import Array
+from pygama.lgdo.vectorofvectors import VectorOfVectors
+from pygama.lgdo.scalar import Scalar
+from pygama.lgdo.struct import Struct
+
+LGDO = Array | Scalar | Struct | VectorOfVectors
+
+log = logging.getLogger(__name__)
 
 
 class Table(Struct):
-    """
-    A special struct of array or subtable "columns" of equal length.
+    """A special struct of arrays or subtable columns of equal length.
 
-    Holds onto an internal read/write location "loc" that is useful in managing
-    table I/O using functions like push_row(), is_full(), and clear()
+    Holds onto an internal read/write location ``loc`` that is useful in
+    managing table I/O using functions like :meth:`push_row`, :meth:`is_full`,
+    and :meth:`clear`.
 
-    Note: if you write to a table and don't fill it up to its total size, be
-    sure to resize it before passing to data processing functions, as they will
-    call len(table) to access valid data, which returns table.size.
+    Note
+    ----
+    If you write to a table and don't fill it up to its total size, be sure to
+    resize it before passing to data processing functions, as they will call
+    :meth:`__len__` to access valid data, which returns the ``size`` attribute.
     """
     # TODO: overload getattr to allow access to fields as object attributes?
 
-
-    def __init__(self, size=None, col_dict={}, attrs={}):
-        """
+    def __init__(self, size: int = None, col_dict: dict[str, LGDO] = {},
+                 attrs: dict[str, Any] = {}) -> None:
+        r"""
         Parameters
         ----------
-        size : int (optional)
-            Sets the number of rows in the table. Arrays in col_dict will be
-            resized to match size if both are not None.  If size is left as
-            None, the number of table rows is determined from the length of the
-            first array in col_dict. If neither is provided, a default length of
-            1024 is used.
-        col_dict : dict (optional)
-            Instantiate this table using the supplied named array-like lgdo's.
+        size
+            sets the number of rows in the table. :class:`~.Array`\ s in
+            `col_dict will be resized to match size if both are not ``None``.
+            If `size` is left as ``None``, the number of table rows is
+            determined from the length of the first array in `col_dict`. If
+            neither is provided, a default length of 1024 is used.
+        col_dict
+            instantiate this table using the supplied named array-like LGDO's.
             Note 1: no copy is performed, the objects are used directly.
-            Note 2: if size is not None, all arrays will be resized to match it
-            Note 3: if the arrays have different lengths, all will be resized to
-            match the length of the first array
-        attrs : dict (optional)
-            A set of user attributes to be carried along with this lgdo
+            Note 2: if `size` is not ``None``, all arrays will be resized to
+            match it.  Note 3: if the arrays have different lengths, all will
+            be resized to match the length of the first array.
+        attrs
+            A set of user attributes to be carried along with this LGDO.
 
         Notes
         -----
-        self.loc is initialized to 0
+        the :attr:`loc` attribute is initialized to 0.
         """
         super().__init__(obj_dict=col_dict, attrs=attrs)
 
@@ -52,63 +69,62 @@ class Table(Struct):
             self.resize(new_size=size, do_warn=do_warn)
 
         # if no col_dict, just set the size (default to 1024)
-        else: self.size = size if size is not None else 1024
+        else:
+            self.size = size if size is not None else 1024
 
         # always start at loc=0
         self.loc = 0
 
-
-    def datatype_name(self):
-        """The name for this lgdo's datatype attribute"""
+    def datatype_name(self) -> str:
+        """The name for this LGDO's datatype attribute."""
         return 'table'
 
-
-    def __len__(self):
-        """Provides __len__ for this array-like class"""
+    def __len__(self) -> int:
+        """Provides ``__len__`` for this array-like class."""
         return self.size
 
-
-    def resize(self, new_size = None, do_warn = False):
+    def resize(self, new_size: int = None, do_warn: bool = False) -> None:
         # if new_size = None, use the size from the first field
         for field, obj in self.items():
-            if new_size is None: new_size = len(obj)
+            if new_size is None:
+                new_size = len(obj)
             elif len(obj) != new_size:
                 if do_warn:
-                    print('warning: resizing field', field,
-                          'with size', len(obj), '!=', new_size)
-                if isinstance(obj, Table): obj.resize(new_size, do_warn)
-                else: obj.resize(new_size)
+                    log.warning(f'warning: resizing field {field}',
+                                f'with size {len(obj)} != {new_size}')
+                if isinstance(obj, Table):
+                    obj.resize(new_size)
+                else:
+                    obj.resize(new_size)
         self.size = new_size
 
-
-    def push_row(self):
+    def push_row(self) -> None:
         self.loc += 1
 
-
-    def is_full(self):
+    def is_full(self) -> bool:
         return self.loc >= self.size
 
-
-    def clear(self):
+    def clear(self) -> None:
         self.loc = 0
 
+    def add_field(self, name: str, obj: LGDO,
+                  use_obj_size: bool = False, do_warn=True) -> None:
+        """Add a field (column) to the table.
 
-    def add_field(self, name, obj, use_obj_size=False, do_warn=True):
-        """ Add a field (column) to the table.
-
-        Use the name "field" here to match the terminology used in Struct.
+        Use the name "field" here to match the terminology used in
+        :class:`.Struct`.
 
         Parameters
         ----------
-        name : str
+        name
             the name for the field in the table.
-        obj : lgdo
-            the object to be added to the table
-        use_obj_size : bool (optional)
-            if true, resize the table to match the length of obj
-        do_warn : bool (optional)
-            print or don't print useful info. Passed to self.resize() when
-            use_obj_size is True
+        obj
+            the object to be added to the table.
+        use_obj_size
+            if ``True``, resize the table to match the length of `obj`.
+        do_warn
+            print or don't print useful info. Passed to :meth:`resize` when
+            `use_obj_size` is ``True``.
         """
         if not hasattr(obj, '__len__'):
             raise TypeError('cannot add field of type', type(obj).__name__)
@@ -118,63 +134,66 @@ class Table(Struct):
         # check / update sizes
         if self.size != len(obj):
             new_size = len(obj) if use_obj_size else self.size
-            self.resize(new_size=new_size, do_warn=do_warn)
+            self.resize(new_size=new_size)
 
-
-    def add_column(self, name, obj, use_obj_size=False, do_warn=True):
-        """Alias for add_field using table terminology 'column'"""
+    def add_column(self, name: str, obj: LGDO, use_obj_size: bool = False,
+                   do_warn: bool = True) -> None:
+        """Alias for :meth:`.add_field` using table terminology 'column'."""
         self.add_field(name, obj, use_obj_size=use_obj_size, do_warn=do_warn)
 
+    def join(self, other_table: Table, cols: list[str] = None,
+             do_warn: bool = True) -> None:
+        """Add the columns of another table to this table.
 
-    def join(self, other_table, cols=None, do_warn=True):
-        """Add the columns of other_table to this table
-
-        Note: following the join, both tables have access to other_table's
-        fields (but other_table doesn't have access to this table's fields). No
-        memory is allocated in this process. Other_table can go out of scope and
-        this table will retain access to the joined data.
+        Note
+        ----
+        Following the join, both tables have access to `other_table`'s fields
+        (but `other_table` doesn't have access to this table's fields). No
+        memory is allocated in this process. `other_table` can go out of scope
+        and this table will retain access to the joined data.
 
         Parameters
         ----------
-        other_table : Table
-            the table whose columns are to be joined into this table
-        cols : list or None (optional)
-            a list of names of columns from other_table to be joined into this
-            table
-        do_warn : bool (optional)
-            set to False to turn off error warnings associated with mismatched
-            loc parameter or add_column() warnings
+        other_table
+            the table whose columns are to be joined into this table.
+        cols
+            a list of names of columns from `other_table` to be joined into
+            this table.
+        do_warn
+            set to ``False`` to turn off warnings associated with mismatched
+            `loc` parameter or :meth:`add_column` warnings.
         """
         if other_table.loc != self.loc and do_warn:
-            print(f'warning: other_table.loc ({other_table.loc}) != self.loc({self.loc})')
-        if cols is None: cols = other_table.keys()
+            log.warning(f"other_table.loc ({other_table.loc}) != self.loc({self.loc})")
+        if cols is None:
+            cols = other_table.keys()
         for name in cols:
             self.add_column(name, other_table[name], do_warn=do_warn)
 
+    def get_dataframe(self, cols: list[str] = None,
+                      copy: bool = False) -> pd.DataFrame:
+        """Get a :class:`pandas.DataFrame` from the data in the table.
 
-    def get_dataframe(self, cols=None, copy=False):
-        """Get a pandas dataframe from the data in the table.
-
-        Note: the requested data must be array-like, with the nda attribute.
+        Note
+        ----
+        The requested data must be array-like, with the ``nda`` attribute.
 
         Parameters
         ----------
-        cols : list of strs (optional)
-            A list of column names specifying the subset of the table's columns
-            to be added to the dataframe
-        copy : bool (optional)
-            When true, the dataframe allocates new memory and copies data into
-            it. Otherwise, the raw nda's from the table are used directly.
-
-        Returns
-        -------
-        dataframe : pandas DataFrame
+        cols
+            a list of column names specifying the subset of the table's columns
+            to be added to the dataframe.
+        copy
+            When ``True``, the dataframe allocates new memory and copies data
+            into it. Otherwise, the raw ``nda``'s from the table are used directly.
         """
         df = pd.DataFrame(copy=copy)
-        if cols is None: cols = self.keys()
+        if cols is None:
+            cols = self.keys()
         for col in cols:
-            if not hasattr(self[col],'nda'):
-                print(f'column {col} does not have an nda, skipping...')
-            else: df[col] = self[col].nda
+            if not hasattr(self[col], 'nda'):
+                log.warning(f'column {col} does not have an nda, skipping...')
+            else:
+                df[col] = self[col].nda
 
         return df
