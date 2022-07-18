7610612cdd376fea684d31cb383d6d20ada6115e
Patrick Krause <pkrause@lfs1.mpi-hd.mpg.de>
2022-07-18 19:57:59 +0200

    add idx_out parameter to bl_from_y_projection processors
    (cherry picked from commit 14ab2b55f1db436b54ef487a0c69723a85495575)

--- src/pygama/dsp/processors/bl_from_y_projection.py
+++ src/pygama/dsp/processors/bl_from_y_projection.py
@@ -7,13 +7,13 @@ from pygama.dsp.errors import DSPFatal
 
 # 2) Provide instructions to Numba
 
-@guvectorize(["void(float32[:], float32[:], float32[:], float32[:])",
-              "void(float64[:], float64[:], float64[:], float64[:])"],
-              "(n),(n),(),()", nopython=True, cache=True)
+@guvectorize(["void(float32[:], float32[:], float32[:], float32[:],float32[:])",
+              "void(float64[:], float64[:], float64[:], float64[:],float64[:])"],
+              "(n),(n),(),(),()", nopython=True, cache=True)
 
 # 3) Define the processor interface
 
-def bl_from_y_projection(proj_in, border_in, mean_out, std_out):
+def bl_from_y_projection(proj_in, border_in, mean_out, std_out,idx_out):
 
     # 4) Document the algorithm
     
@@ -31,12 +31,16 @@ def bl_from_y_projection(proj_in, border_in, mean_out, std_out):
     
     std_out : scalar
         Returns the standard deviation of the maximum of the projection (this should be the baseline standard deviation)
+    
+    idx_out : scalar
+        Returns the index of the max. position found in the projection (proj_in[idx_out] = mean_out)
     """
 
     # 5) Initialize output parameters
     
     mean_out[0] = np.nan
     std_out[0] = np.nan 
+    idx_out[0] = np.nan 
 
 
     # 6) Check inputs
@@ -52,6 +56,7 @@ def bl_from_y_projection(proj_in, border_in, mean_out, std_out):
     for i in range(0, len(proj_in), 1):
         if proj_in[i] > proj_in[max_index]:
             max_index = i
+    idx_out[0] = max_index
     mean_out[0] = border_in[max_index]
 
     #and the approx standarddev
