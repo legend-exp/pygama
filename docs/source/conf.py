fe542ce8d8aa0cd5b5772d12988f5fcf472afc5e
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-08-02 18:45:03 +0200

    [docs] remove unneeded sphinx-multiversion dependency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,6 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
-    "sphinx_multiversion",
     "sphinx_copybutton",
     "myst_parser",
 ]
@@ -93,27 +92,5 @@ intersphinx_mapping = {
 # Include __init__() docstring in class docstring
 autoclass_content = "both"
 autodoc_typehints = "both"
-autodoc_typehints_description_target = "documented"
+autodoc_typehints_description_target = "documented_params"
 autodoc_typehints_format = "short"
-
-# sphinx-multiversion
-
-# For now, we include only (certain) branches when building docs.
-# To add a specific release to the list of versions for which docs should be build,
-# one must create a new branch named `releases/...`
-smv_branch_whitelist = r"^(main|refactor|releases/.*)$"
-smv_tag_whitelist = "^$"
-smv_released_pattern = "^$"
-smv_outputdir_format = "{ref.name}"
-smv_prefer_remote_refs = False
-
-# HACK: we need to regenerate the API documentation before the actual build,
-# but it's not possible with the current sphinx-multiversion. Changes have been
-# proposed in this PR: https://github.com/Holzhaus/sphinx-multiversion/pull/62
-# but there's no timeline for merging yet. For the following option to be considered,
-# one needs to install the sphinx-multiversion-pre-post-build fork from PyPI
-smv_prebuild_command = "make -ik apidoc"
-
-# The right way to find all docs versions is to look for matching branches on
-# the default remote
-smv_remote_whitelist = r"^origin$"
