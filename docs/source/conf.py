498535fdf95de1535b5393b036fb92b4d0a13118
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-08-03 10:30:26 +0200

    [docs] switch to ReadTheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,6 @@
 
 import pathlib
 import sys
-import os
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 project = 'pygama'
@@ -13,74 +12,79 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
     'sphinx_multiversion',
     'myst_parser'
 ]
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
-master_doc = 'index'
-language = 'python'
-templates_path = ['_templates']
-pygments_style = 'sphinx'
-html_theme = 'sphinx_rtd_theme'
+master_doc = "index"
+language = "python"
+# in _templates/ we have a custom layout.html to include the version menu
+# (adapted from sphinx-multiversion docs)
+templates_path = ["_templates"]
+
+# Furo theme
+html_theme = "furo"
+html_theme_options = {
+    "source_repository": "https://github.com/legend-exp/pygama",
+    "source_branch": "main",
+    "source_directory": "docs/source",
+}
+html_title = f"{project}"
 
 # list here pygama dependencies that are not required for building docs and
 # could be unmet at build time
 autodoc_mock_imports = [
-    'pandas',
-    'numpy',
-    'matplotlib',
-    'mplhep',
-    'scimath',
-    'numba',
-    'pytest',
-    'pyhf',
-    'awkward',
-    'iminuit',
-    'boost-histogram',
-    'hepunits',
-    'hepstats',
-    'uproot',
-    'h5py',
-    'pint',
-    'pyfftw',
-    'tqdm',
-    'tinydb',
-    'parse'
+    "pygama._version",
+    "pandas",
+    # 'numpy',
+    "matplotlib",
+    "mplhep",
+    "scipy",
+    "numba",
+    "pytest",
+    "pyhf",
+    "awkward",
+    "iminuit",
+    "boost-histogram",
+    "hepunits",
+    "hepstats",
+    "uproot",
+    "h5py",
+    "pint",
+    "pyfftw",
+    "tqdm",
+    "tinydb",
+    "parse",
 ]
+autodoc_default_options = {"ignore-module-all": True}
 
 # sphinx-napoleon
 # enforce consistent usage of NumPy-style docstrings
 napoleon_numpy_docstring = True
 napoleon_google_docstring = False
+napoleon_use_ivar = True
+napoleon_custom_sections = ["JSON Configuration Example"]
+
+# intersphinx
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "numba": ("https://numba.readthedocs.io/en/stable", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy", None),
+    "pandas": ("https://pandas.pydata.org/docs", None),
+    "matplotlib": ("https://matplotlib.org/stable", None),
+    "iminuit": ("https://iminuit.readthedocs.io/en/stable", None),
+    "h5py": ("https://docs.h5py.org/en/stable", None),
+    "pint": ("https://pint.readthedocs.io/en/stable", None),
+}
 
 # sphinx-autodoc
 # Include __init__() docstring in class docstring
-autoclass_content = 'both'
-
-# sphinx-multiversion
-
-# For now, we include only (certain) branches when building docs.
-# To add a specific release to the list of versions for which docs should be build,
-# one must create a new branch named `releases/...`
-smv_branch_whitelist = r'^(main|refactor|releases/.*)$'
-smv_tag_whitelist = '^$'
-smv_released_pattern = '^$'
-smv_outputdir_format = '{ref.name}'
-smv_prefer_remote_refs = False
-
-# HACK: we need to regenerate the API documentation before the actual build,
-# but it's not possible with the current sphinx-multiversion. Changes have been
-# proposed in this PR: https://github.com/Holzhaus/sphinx-multiversion/pull/62
-# but there's no timeline for merging yet. For the following option to be considered,
-# one needs to install sphinx-multiversion from a fork with the following:
-# $ pip install git+https://github.com/samtygier-stfc/sphinx-multiversion.git@prebuild_command
-smv_prebuild_command = 'make -ik apidoc'
-
-# The right way to find all docs versions is to look for matching branches on
-# the default remote
-smv_remote_whitelist = r'^origin$'
+autoclass_content = "both"
+autodoc_typehints = "both"
+autodoc_typehints_description_target = "documented_params"
+autodoc_typehints_format = "short"
