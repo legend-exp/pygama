148524111fe42b84fd44ddd5e0f27494f8e6573f
George Marshall <marshall@legend-login2.lngs.infn.it>
2022-08-05 17:12:34 +0200

    Bugfixes

--- src/pygama/math/peak_fitting.py
+++ src/pygama/math/peak_fitting.py
@@ -169,7 +169,7 @@ def fit_unbinned(func, data, guess=None,
     m.hesse()
     return m.values, m.errors, m.covariance
 
-def goodness_of_fit(hist, bins, var, func, pars, method='var'):
+def goodness_of_fit(hist, bins, var, func, pars, method='var', scale_bins=False):
     """Compute chisq and dof of fit
 
     Parameters
@@ -207,7 +207,9 @@ def goodness_of_fit(hist, bins, var, func, pars, method='var'):
 
 
     # compute expected values
-    yy = func(pgh.get_bin_centers(bins), *pars) * pgh.get_bin_widths(bins)
+    yy = func(pgh.get_bin_centers(bins), *pars) 
+    if scale_bins ==True:
+        yy*= pgh.get_bin_widths(bins)
 
     if method == 'LR':
         log_lr = 2*np.sum(np.where(hist>0 , yy-hist + hist*np.log((hist+1.e-99) / (yy+1.e-99)), yy-hist))
@@ -811,7 +813,7 @@ def radford_fwhm(sigma, htail, tau,  cov = None):
         return -gauss_with_tail_pdf(np.array([E]), 0, sigma, htail, tau)[0]
 
     if htail<0 or htail>1:
-        print("htail outside allowed limits of 0 and 1")
+        #print("htail outside allowed limits of 0 and 1")
         raise ValueError
 
     res = minimize_scalar( neg_radford_peak_bgfree,
