e043908d812ccaefe87fa149e51ed6586bf8282d
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-08-09 11:39:44 +0200

    [hit] first draft implementation of build_hit and lgdo.Table.eval

--- src/pygama/hit/__init__.py
+++ src/pygama/hit/__init__.py
@@ -1,3 +1,8 @@
 """
-Subpackage description.
+Routines for applying columnar transformations to tabular data. Specifically,
+to produce the hit-tier from the dsp-tier.
 """
+
+from pygama.hit.build_hit import build_hit
+
+__all__ = ["build_hit"]
---
+++ tests/hit/test_build_hit.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+config_dir = Path(__file__).parent / "configs"
