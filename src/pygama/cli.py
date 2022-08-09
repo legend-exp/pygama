4b58d230a1e5d2d0bc8f3fe76fcb2c3381044682
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-08-09 21:44:34 +0200

    [cli] add build-hit

--- src/pygama/cli.py
+++ src/pygama/cli.py
@@ -9,6 +9,7 @@ import sys
 import numpy as np
 
 import pygama
+from pygama.hit import build_hit
 from pygama.dsp import build_dsp
 from pygama.raw import build_raw
 
@@ -47,6 +48,7 @@ def pygama_cli():
 
     add_build_raw_parser(subparsers)
     add_build_dsp_parser(subparsers)
+    add_build_hit_parser(subparsers)
 
     if len(sys.argv) < 2:
         parser.print_usage(sys.stderr)
@@ -277,4 +279,121 @@ def build_dsp_cli(args):
             write_mode=args.writemode,
             buffer_len=args.chunk,
             block_width=args.block,
+        )
+
+
+def add_build_hit_parser(subparsers):
+    """Configure :func:`.hit.build_hit.build_hit` command line interface"""
+
+    parser_r2d = subparsers.add_parser(
+        "build-hit",
+        description="""Process LH5 dsp files and produce a
+        hit file using a JSON configuration""",
+    )
+    parser_r2d.add_argument(
+        "dsp_lh5_file",
+        nargs="+",
+        help="""Input dsp LH5 file. Can be a single file or
+                            a list of them""",
+    )
+    parser_r2d.add_argument(
+        "--config",
+        "-c",
+        required=True,
+        help=""""JSON file holding configuration of column operations on DSP output""",
+    )
+    parser_r2d.add_argument(
+        "--hdf5-groups",
+        "-g",
+        nargs="*",
+        default=None,
+        help="""Name of group in the LH5 file. By default
+                            process all base groups. Supports wildcards""",
+    )
+    parser_r2d.add_argument(
+        "--output",
+        "-o",
+        default=None,
+        help="""Name of output file, if only one is
+                            supplied. By default, output to
+                            <input-filename>_hit.lh5""",
+    )
+    parser_r2d.add_argument(
+        "--max-rows",
+        "-n",
+        default=None,
+        type=int,
+        help="""Number of rows to process. By default
+                            do the whole file""",
+    )
+    parser_r2d.add_argument(
+        "--chunk",
+        "-k",
+        default=3200,
+        type=int,
+        help="""Number of waveforms to read from disk at a
+                            time. Default is 3200""",
+    )
+
+    group = parser_r2d.add_mutually_exclusive_group()
+    group.add_argument(
+        "--overwrite",
+        "-w",
+        action="store_const",
+        const="of",
+        dest="writemode",
+        default="w",
+        help="""Overwrite file if it already exists. Default
+                       option""",
+    )
+    group.add_argument(
+        "--update",
+        "-u",
+        action="store_const",
+        const="u",
+        dest="writemode",
+        help="""Update existing file with new values""",
+    )
+    group.add_argument(
+        "--append",
+        "-a",
+        action="store_const",
+        const="a",
+        dest="writemode",
+        help="""Append values to existing
+                       file""",
+    )
+
+    parser_r2d.set_defaults(func=build_hit_cli)
+
+
+def build_hit_cli(args):
+    """Passes command line arguments to :func:`.hit.build_hit.build_hit`."""
+
+    if len(args.dsp_lh5_file) > 1 and args.output is not None:
+        raise NotImplementedError("not possible to set multiple output file names yet")
+
+    out_files = []
+    if len(args.dsp_lh5_file) == 1:
+        if args.output is None:
+            basename = os.path.splitext(os.path.basename(args.dsp_lh5_file[0]))[0]
+            basename = basename.removesuffix("_dsp")
+            out_files.append(f"{basename}_hit.lh5")
+        else:
+            out_files.append(args.output)
+    else:
+        for file in args.dsp_lh5_file:
+            basename = os.path.splitext(os.path.basename(file))[0]
+            basename = basename.removesuffix("_dsp")
+            out_files.append(f"{basename}_hit.lh5")
+
+    for i in range(len(args.dsp_lh5_file)):
+        build_hit(
+            args.dsp_lh5_file[i],
+            args.config,
+            outfile=out_files[i],
+            lh5_tables=args.hdf5_groups,
+            n_max=args.max_rows,
+            wo_mode=args.writemode,
+            buffer_len=args.chunk,
         )
