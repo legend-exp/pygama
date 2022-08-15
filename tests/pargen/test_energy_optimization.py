dce95b1051de43bfaba6c1247a6ecaf78edfd19e
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-08-15 10:10:30 +0200

    Add a couple of tests to debug import error

---
+++ tests/pargen/test_energy_optimization.py
@@ -0,0 +1,11 @@
+from pygama.pargen import energy_optimisation  # noqa: F401
+import scipy
+
+
+def test_import():
+    pass
+
+
+def test_scipy_version():
+    assert scipy.__version__ != ""
+    assert scipy.__version__ is not None
