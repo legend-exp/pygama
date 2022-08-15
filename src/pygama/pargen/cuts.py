073043e7c8aa0953e008432cbda484e10d34b15a
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-08-15 15:29:45 +0200

    [pre-commit] run black on pargen directory

--- src/pygama/pargen/cuts.py
+++ src/pygama/pargen/cuts.py
@@ -4,24 +4,25 @@ This module provides routines for calculating and applying quality cuts
 
 from __future__ import annotations
 
+import glob
+import json
+import logging
+import os
+
 import numpy as np
 import pandas as pd
-import os
-import json
-import glob
-import logging
 
-import pygama.math.peak_fitting as pgf
-import pygama.math.histogram as pgh
 import pygama.lgdo.lh5_store as lh5
+import pygama.math.histogram as pgh
+import pygama.math.peak_fitting as pgf
 
 log = logging.getLogger(__name__)
 
 
-def generate_cuts(data:dict[str, np.ndarray], parameters:list[str])->dict:
+def generate_cuts(data: dict[str, np.ndarray], parameters: list[str]) -> dict:
     """
-    Finds double sided cut boundaries for a file for the parameters specified 
-    
+    Finds double sided cut boundaries for a file for the parameters specified
+
     Parameters
     ----------
     data : lh5 table or dictionary of arrays
@@ -36,55 +37,57 @@ def generate_cuts(data:dict[str, np.ndarray], parameters:list[str])->dict:
         par_array = data[par]
         if not isinstance(par_array, np.ndarray):
             par_array = par_array.nda
-        counts, start_bins, var = pgh.get_hist(par_array,10**5)
+        counts, start_bins, var = pgh.get_hist(par_array, 10**5)
         max_idx = np.argmax(counts)
         mu = start_bins[max_idx]
-        bin_range=1000
-        
-        if max_idx <bin_range:
+        bin_range = 1000
+
+        if max_idx < bin_range:
             lower_bound_idx = 0
         else:
-            lower_bound_idx = max_idx-bin_range
+            lower_bound_idx = max_idx - bin_range
         lower_bound = start_bins[lower_bound_idx]
-        
-        if max_idx >len(start_bins)-bin_range:
+
+        if max_idx > len(start_bins) - bin_range:
             upper_bound_idx = -1
         else:
-            upper_bound_idx = max_idx+bin_range
-        
+            upper_bound_idx = max_idx + bin_range
+
         upper_bound = start_bins[upper_bound_idx]
-        
+
         try:
-            counts, bins, var = pgh.get_hist(par_array,bins = 1000, range = (lower_bound, upper_bound))
-            
+            counts, bins, var = pgh.get_hist(
+                par_array, bins=1000, range=(lower_bound, upper_bound)
+            )
+
             bin_centres = pgh.get_bin_centers(bins)
-            
+
             fwhm = pgh.get_fwhm(counts, bins)[0]
             mean = float(bin_centres[np.argmax(counts)])
-            std = fwhm/2.355
+            std = fwhm / 2.355
         except IndexError:
-            bin_range=5000
-        
-            if max_idx <bin_range:
+            bin_range = 5000
+
+            if max_idx < bin_range:
                 lower_bound_idx = 0
             else:
-                lower_bound_idx = max_idx-bin_range
+                lower_bound_idx = max_idx - bin_range
             lower_bound = start_bins[lower_bound_idx]
-            
-            if max_idx >len(start_bins)-bin_range:
+
+            if max_idx > len(start_bins) - bin_range:
                 upper_bound_idx = -1
             else:
-                upper_bound_idx = max_idx+bin_range
+                upper_bound_idx = max_idx + bin_range
             upper_bound = start_bins[upper_bound_idx]
-            counts, bins, var = pgh.get_hist(par_array,bins = 1000, range = (lower_bound, upper_bound))
-            
+            counts, bins, var = pgh.get_hist(
+                par_array, bins=1000, range=(lower_bound, upper_bound)
+            )
+
             bin_centres = pgh.get_bin_centers(bins)
-            
+
             fwhm = pgh.get_fwhm(counts, bins)[0]
             mean = float(bin_centres[np.argmax(counts)])
-            std = fwhm/2.355
-        
-        
+            std = fwhm / 2.355
 
         if isinstance(num_sigmas, (int, float)):
             num_sigmas_left = num_sigmas
@@ -92,15 +95,23 @@ def generate_cuts(data:dict[str, np.ndarray], parameters:list[str])->dict:
         elif isinstance(num_sigmas, dict):
             num_sigmas_left = num_sigmas["left"]
             num_sigmas_right = num_sigmas["right"]
-        upper =float( (num_sigmas_right *std)+mean)
-        lower = float((-num_sigmas_left *std)+mean)
-        output_dict[par] ={'Mean Value': mean, 'Sigmas Cut': num_sigmas, 'Upper Boundary' : upper, 'Lower Boundary': lower}
+        upper = float((num_sigmas_right * std) + mean)
+        lower = float((-num_sigmas_left * std) + mean)
+        output_dict[par] = {
+            "Mean Value": mean,
+            "Sigmas Cut": num_sigmas,
+            "Upper Boundary": upper,
+            "Lower Boundary": lower,
+        }
     return output_dict
 
-def get_cut_indexes(all_data:dict[str, np.ndarray], cut_dict:dict, energy_param:str = 'trapTmax')->list[int]:
+
+def get_cut_indexes(
+    all_data: dict[str, np.ndarray], cut_dict: dict, energy_param: str = "trapTmax"
+) -> list[int]:
 
     """
-    Returns a mask of the data, for a single file, that passes cuts based on dictionary of cuts 
+    Returns a mask of the data, for a single file, that passes cuts based on dictionary of cuts
     in form of cut boundaries above
     Parameters
     ----------
@@ -109,35 +120,39 @@ def get_cut_indexes(all_data:dict[str, np.ndarray], cut_dict:dict, energy_param:
     Cut_dict : string
                 Dictionary file with cuts
     """
-    
+
     indexes = None
     keys = cut_dict.keys()
     for cut in keys:
         data = all_data[cut]
         if not isinstance(data, np.ndarray):
             data = data.nda
-        
-        if 'Energy Dep' in cut_dict[cut].keys():
-            energy_midpoints, uppers, lowers = get_energy_dep(all_data, cut, 
-                                                              n_sigmas= cut_dict[cut]['Energy Dep']['Cuts Specified'],
-                                                               energy_param= energy_param, n_windows=10, verbose=verbose)
+
+        if "Energy Dep" in cut_dict[cut].keys():
+            energy_midpoints, uppers, lowers = get_energy_dep(
+                all_data,
+                cut,
+                n_sigmas=cut_dict[cut]["Energy Dep"]["Cuts Specified"],
+                energy_param=energy_param,
+                n_windows=10,
+                verbose=verbose,
+            )
             pars = np.polynomial.polynomial.polyfit(energy_midpoints, uppers, deg=2)
             pars2 = np.polynomial.polynomial.polyfit(energy_midpoints, lowers, deg=2)
-            upper_bounds = np.polynomial.polynomial.polyval(all_data['trapEmax'], pars)
-            lower_bounds = np.polynomial.polynomial.polyval(all_data['trapEmax'], pars2)          
-            idxs = (data<upper_bounds)& (data>lower_bounds)
+            upper_bounds = np.polynomial.polynomial.polyval(all_data["trapEmax"], pars)
+            lower_bounds = np.polynomial.polynomial.polyval(all_data["trapEmax"], pars2)
+            idxs = (data < upper_bounds) & (data > lower_bounds)
         else:
-            upper = cut_dict[cut]['Upper Boundary']
-            lower = cut_dict[cut]['Lower Boundary']
-            idxs = (data<upper) & (data>lower) 
-        
+            upper = cut_dict[cut]["Upper Boundary"]
+            lower = cut_dict[cut]["Lower Boundary"]
+            idxs = (data < upper) & (data > lower)
+
         # Combine masks
         if indexes is not None:
             indexes = indexes & idxs
-            
+
         else:
             indexes = idxs
-        log.debug(f'{cut} loaded')
+        log.debug(f"{cut} loaded")
 
     return indexes
-
--- src/pygama/pargen/dsp_optimize.py
+++ src/pygama/pargen/dsp_optimize.py
@@ -8,7 +8,9 @@ import numpy as np
 from pygama.dsp import build_processing_chain
 
 
-def run_one_dsp(tb_data, dsp_config, db_dict=None, fom_function=None, verbosity=0, fom_kwargs=None):
+def run_one_dsp(
+    tb_data, dsp_config, db_dict=None, fom_function=None, verbosity=0, fom_kwargs=None
+):
     """
     run one iteration of DSP on tb_data
 
@@ -43,31 +45,36 @@ def run_one_dsp(tb_data, dsp_config, db_dict=None, fom_function=None, verbosity=
         If fom_function is None, returns the output lh5 table for the DSP iteration
     """
 
-    pc, lh5_col_names, tb_out = build_processing_chain(tb_data, dsp_config, db_dict=db_dict)
+    pc, lh5_col_names, tb_out = build_processing_chain(
+        tb_data, dsp_config, db_dict=db_dict
+    )
     pc.execute()
     if fom_function is not None:
         if fom_kwargs is not None:
             return fom_function(tb_out, verbosity, fom_kwargs)
         else:
             return fom_function(tb_out, verbosity)
-    else: return tb_out
+    else:
+        return tb_out
 
 
-ParGridDimension = namedtuple('ParGridDimension', 'name parameter value_strs')
+ParGridDimension = namedtuple("ParGridDimension", "name parameter value_strs")
 
-class ParGrid():
-    """ Parameter Grid class
+
+class ParGrid:
+    """Parameter Grid class
     Each ParGrid entry corresponds to a dsp parameter to be varied.
     The ntuples must follow the pattern:
     ( name parameter value_strs) : ( str, str, list of str)
     where name and parameter are the same as 'db.name.parameter' in the processing chain,
     value_strs is the array of strings to set the argument to.
     """
+
     def __init__(self):
         self.dims = []
 
     def add_dimension(self, name, parameter, value_strs):
-        self.dims.append( ParGridDimension(name, parameter, value_strs) )
+        self.dims.append(ParGridDimension(name, parameter, value_strs))
 
     def get_n_dimensions(self):
         return len(self.dims)
@@ -85,7 +92,7 @@ class ParGrid():
         return np.prod(self.get_shape())
 
     def get_par_meshgrid(self, copy=False, sparse=False):
-        """ return a meshgrid of parameter values
+        """return a meshgrid of parameter values
         Always uses Matrix indexing (natural for par grid) so that
         mg[i1][i2][...] corresponds to index order in self.dims
         Note copy is False by default as opposed to numpy default of True
@@ -93,13 +100,13 @@ class ParGrid():
         axes = []
         for i in range(self.get_n_dimensions()):
             axes.append(self.dims[i].values_strs)
-        return np.meshgrid(*axes, copy, sparse, indexing='ij')
+        return np.meshgrid(*axes, copy, sparse, indexing="ij")
 
     def get_zero_indices(self):
         return np.zeros(self.get_n_dimensions(), dtype=np.uint32)
 
     def iterate_indices(self, indices):
-        """ iterate given indices [i1, i2, ...] by one.
+        """iterate given indices [i1, i2, ...] by one.
         For easier iteration. The convention here is arbitrary, but its the
         order the arrays would be traversed in a series of nested for loops in
         the order appearin in dims (first dimension is first for loop, etc):
@@ -107,11 +114,12 @@ class ParGrid():
         """
         for iD in reversed(range(self.get_n_dimensions())):
             indices[iD] += 1
-            if indices[iD] < self.get_n_points_of_dim(iD): return True
+            if indices[iD] < self.get_n_points_of_dim(iD):
+                return True
             indices[iD] = 0
         return False
 
-    #def check_indices(self, indices):
+    # def check_indices(self, indices):
     #    for iD in reversed(range(self.get_n_dimensions())):
     #        if indices[iD] < self.get_n_points_of_dim(iD): return True
     #        indices[iD] = 0
@@ -133,15 +141,17 @@ class ParGrid():
         if db_dict is None:
             db_dict = {}
         for i_dim, i_par in enumerate(indices):
-            name, parameter, value_str= self.get_data(i_dim, i_par)
+            name, parameter, value_str = self.get_data(i_dim, i_par)
             if name not in db_dict.keys():
-                db_dict[name] = {parameter:value_str}
+                db_dict[name] = {parameter: value_str}
             else:
                 db_dict[name][parameter] = value_str
         return db_dict
 
 
-def run_grid(tb_data, dsp_config, grid, fom_function, db_dict=None, verbosity=1, **fom_kwargs):
+def run_grid(
+    tb_data, dsp_config, grid, fom_function, db_dict=None, verbosity=1, **fom_kwargs
+):
     """Extract a table of optimization values for a grid of DSP parameters
     The grid argument defines a list of parameters and values over which to run
     the DSP defined in dsp_config on tb_data. At each point, a scalar
@@ -179,58 +189,75 @@ def run_grid(tb_data, dsp_config, grid, fom_function, db_dict=None, verbosity=1,
         of the grid argument
     """
 
-    grid_values = np.ndarray(shape=grid.get_shape(), dtype='O')
+    grid_values = np.ndarray(shape=grid.get_shape(), dtype="O")
     iii = grid.get_zero_indices()
-    if verbosity > 0: print("starting grid calculations...")
+    if verbosity > 0:
+        print("starting grid calculations...")
     while True:
         db_dict = grid.set_dsp_pars(db_dict, iii)
-        if verbosity > 1: pprint(dsp_config)
-        if verbosity > 0: grid.print_data(iii)
-        grid_values[tuple(iii)] = run_one_dsp(tb_data,
-                                              dsp_config,
-                                              db_dict=db_dict,
-                                              fom_function=fom_function,
-                                              verbosity=verbosity, fom_kwargs=fom_kwargs)
-        if verbosity > 0: print("value:", grid_values[tuple(iii)])
-        if not grid.iterate_indices(iii): break
+        if verbosity > 1:
+            pprint(dsp_config)
+        if verbosity > 0:
+            grid.print_data(iii)
+        grid_values[tuple(iii)] = run_one_dsp(
+            tb_data,
+            dsp_config,
+            db_dict=db_dict,
+            fom_function=fom_function,
+            verbosity=verbosity,
+            fom_kwargs=fom_kwargs,
+        )
+        if verbosity > 0:
+            print("value:", grid_values[tuple(iii)])
+        if not grid.iterate_indices(iii):
+            break
     return grid_values
 
-def run_grid_point(tb_data, dsp_config, grids, fom_function, iii, db_dict=None, verbosity=1, fom_kwargs=None):
+
+def run_grid_point(
+    tb_data,
+    dsp_config,
+    grids,
+    fom_function,
+    iii,
+    db_dict=None,
+    verbosity=1,
+    fom_kwargs=None,
+):
     """
     Runs a single grid point for the index specified
     """
     if not isinstance(grids, list):
         grids = [grids]
-    for index, grid in zip(iii,grids):
+    for index, grid in zip(iii, grids):
         db_dict = grid.set_dsp_pars(db_dict, index)
 
-    if verbosity > 1: pprint(dsp_config)
+    if verbosity > 1:
+        pprint(dsp_config)
     if verbosity > 0:
-        [grid.print_data(iii[i]) for i,grid in enumerate(grids)]
-        print(' ')
-    tb_out = run_one_dsp(tb_data,
-                        dsp_config,
-                        db_dict=db_dict,
-                        verbosity=verbosity)
-    res = np.ndarray(shape=len(grids), dtype='O')
+        [grid.print_data(iii[i]) for i, grid in enumerate(grids)]
+        print(" ")
+    tb_out = run_one_dsp(tb_data, dsp_config, db_dict=db_dict, verbosity=verbosity)
+    res = np.ndarray(shape=len(grids), dtype="O")
     if fom_function:
         for i in range(len(grids)):
             if fom_kwargs[i] is not None:
-                if len(fom_function)>1:
+                if len(fom_function) > 1:
                     res[i] = fom_function[i](tb_out, verbosity, fom_kwargs[i])
                 else:
                     res[i] = fom_function[0](tb_out, verbosity, fom_kwargs[i])
             else:
-                if len(fom_function)>1:
+                if len(fom_function) > 1:
                     res[i] = fom_function[i](tb_out, verbosity)
                 else:
                     res[i] = fom_function[0](tb_out, verbosity)
-        print("value:",res)
-        out = {'indexes': [tuple(ii) for ii in iii], 'results': res}
+        print("value:", res)
+        out = {"indexes": [tuple(ii) for ii in iii], "results": res}
 
     else:
-        out = {'indexes': [tuple(ii) for ii in iii], 'results': tb_out}
+        out = {"indexes": [tuple(ii) for ii in iii], "results": tb_out}
     return out
+
 
 def get_grid_points(grid):
     """
@@ -238,20 +265,30 @@ def get_grid_points(grid):
     """
     out = []
     iii = [gri.get_zero_indices() for gri in grid]
-    complete =np.full(len(grid), False)
+    complete = np.full(len(grid), False)
     while True:
         out.append([tuple(ii) for ii in iii])
 
-        for i, gri in enumerate(zip(iii,grid)):
+        for i, gri in enumerate(zip(iii, grid)):
             if not gri[1].iterate_indices(gri[0]):
                 print(f"{i} grid end")
-                iii[i]=gri[1].get_zero_indices()
-                complete[i]=True
-        if all(complete): break
+                iii[i] = gri[1].get_zero_indices()
+                complete[i] = True
+        if all(complete):
+            break
     return out
 
-def run_grid_multiprocess_parallel(tb_data, dsp_config, grid, fom_function, db_dict=None, verbosity=1,
-                          processes=5, fom_kwargs=None):
+
+def run_grid_multiprocess_parallel(
+    tb_data,
+    dsp_config,
+    grid,
+    fom_function,
+    db_dict=None,
+    verbosity=1,
+    processes=5,
+    fom_kwargs=None,
+):
 
     """
     run one iteration of DSP on tb_data with multiprocessing, can handle
@@ -305,26 +342,39 @@ def run_grid_multiprocess_parallel(tb_data, dsp_config, grid, fom_function, db_d
     shapes = [gri.get_shape() for gri in grid]
     if fom_function is not None:
         for i in range(len(grid)):
-            grid_values.append(np.ndarray(shape = shapes[i], dtype='O'))
+            grid_values.append(np.ndarray(shape=shapes[i], dtype="O"))
     else:
         grid_lengths = np.array([gri.get_n_grid_points() for gri in grid])
-        grid_values.append(np.ndarray(shape = shapes[np.argmax(grid_lengths)], dtype='O'))
+        grid_values.append(np.ndarray(shape=shapes[np.argmax(grid_lengths)], dtype="O"))
     grid_list = get_grid_points(grid)
     pool = mp.Pool(processes=processes)
-    results = [pool.apply_async(run_grid_point, args=(tb_data,dsp_config, grid, fom_function, np.asarray(gl),
-                    db_dict, verbosity, fom_kwargs)) for gl in grid_list]
+    results = [
+        pool.apply_async(
+            run_grid_point,
+            args=(
+                tb_data,
+                dsp_config,
+                grid,
+                fom_function,
+                np.asarray(gl),
+                db_dict,
+                verbosity,
+                fom_kwargs,
+            ),
+        )
+        for gl in grid_list
+    ]
 
     for result in results:
         res = result.get()
-        indexes = res['indexes']
+        indexes = res["indexes"]
         if fom_function is not None:
             for i in range(len(grid)):
                 index = indexes[i]
                 if grid_values[i][index] is None:
-                    grid_values[i][index] = res['results'][i]
+                    grid_values[i][index] = res["results"][i]
         else:
-            grid_values[0][indexes[0]] = {f'{indexes[0]}':res['results']}
-
+            grid_values[0][indexes[0]] = {f"{indexes[0]}": res["results"]}
 
     pool.close()
     pool.join()
