3c144d0feca00cec9f7f289e020db9a54aeda7c6
Patrick Krause <p.krause5994@gmail.com>
2022-09-13 14:51:24 +0200

    Bug fix in `build_hit.build_hit()`  (#367)
    Co-authored-by: Patrick Krause <krause@legend-login1.lngs.infn.it>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/pygama/hit/build_hit.py
+++ src/pygama/hit/build_hit.py
@@ -96,6 +96,8 @@ def build_hit(
                 with open(v) as f:
                     # order in hit configs is important (dependencies)
                     tbl_cfg[k] = json.load(f, object_pairs_hook=OrderedDict)
+        lh5_tables_config = tbl_cfg
+
     else:
         if isinstance(hit_config, str):
             # sanitize config
