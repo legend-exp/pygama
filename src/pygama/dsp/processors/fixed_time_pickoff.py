d08fe623670093d1d6bde217e6335fdfe4c5cee7
Ian <iguinn@email.unc.edu>
2022-09-15 13:12:52 -0400

    Removed fixed_sample_pickoff

--- src/pygama/dsp/processors/fixed_time_pickoff.py
+++ src/pygama/dsp/processors/fixed_time_pickoff.py
@@ -9,60 +9,6 @@ from pygama.dsp.utils import numba_defaults_kwargs as nb_kwargs
 
 @guvectorize(
     [
-        "void(float32[:], float32, float32[:])",
-        "void(float64[:], float64, float64[:])",
-        "void(float32[:], int64, float32[:])",
-        "void(float64[:], int64, float64[:])",
-    ],
-    "(n),()->()",
-    **nb_kwargs,
-)
-def fixed_sample_pickoff(w_in: np.ndarray, t_in: int, a_out: np.ndarray) -> None:
-    """
-    Pick off the waveform value at the provided index. If t_in is non-
-    integral, raise an exception. If non-integral t_in is required, use
-    fixed_time_pickoff to interpolate between samples.
-
-    If the provided index `t_in` is out of range, return :any:`numpy.nan`.
-
-    Parameters
-    ----------
-    w_in
-        the input waveform
-    t_in
-        the waveform index to pick off
-    a_out
-        the output pick-off value
-
-    JSON Configuration Example
-    --------------------------
-
-    .. code-block :: json
-
-        "trapEftp": {
-            "function": "fixed_time_pickoff",
-            "module": "pygama.dsp.processors",
-            "args": ["wf_trap", "tp_0+10*us", "trapEftp"],
-            "unit": "ADC"
-        }
-    """
-
-    a_out[0] = np.nan
-
-    if np.isnan(w_in).any() or np.isnan(t_in):
-        return
-
-    if np.floor(t_in) != t_in:
-        raise DSPFatal("The pick-off index must be an integer")
-
-    if int(t_in) < 0 or int(t_in) >= len(w_in):
-        return
-
-    a_out[0] = w_in[int(t_in)]
-
-
-@guvectorize(
-    [
         "void(float32[:], float32, char, float32[:])",
         "void(float64[:], float64, char, float64[:])",
     ],
