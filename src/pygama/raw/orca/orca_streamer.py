e98c46dd5ebf035b5872e1e3745a603d381df556
Jason Detwiler <jasondet@gmail.com>
2023-01-12 14:53:39 -0800

    Only inform, don't error, when decoder not in rb_lib.

--- src/pygama/raw/orca/orca_streamer.py
+++ src/pygama/raw/orca/orca_streamer.py
@@ -247,7 +247,7 @@ class OrcaStreamer(DataStreamer):
         for data_id in self.decoder_id_dict.keys():
             name = id_to_dec_name_dict[data_id]
             if name not in self.rb_lib:
-                raise RuntimeError(f"{name} not in rb_lib")
+                log.info(f"skipping data from {name}")
             self.rbl_id_dict[data_id] = self.rb_lib[name]
             good_buffers.append(name)
         # check that we have instantiated decoders for all buffers
