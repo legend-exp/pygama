1cbfd541b6d82fa55baaae17c21ac18403a57136
Valerio D'Andrea <valerio.dandrea@lngs.infn.it>
2023-03-15 11:15:03 +0100

    Option to pass filter coefficients to DPLMS processor (#462)
    * Added possibility to have filter coefficients as input of the dplms processor
    * update test_dplms
    
    ---------
    
    Co-authored-by: Valerio D'Andrea <dandrea@legend-login1.lngs.infn.it>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/pygama/dsp/processors/dplms.py
+++ src/pygama/dsp/processors/dplms.py
@@ -10,13 +10,14 @@ from pygama.dsp.utils import numba_defaults_kwargs as nb_kwargs
 
 
 def dplms(
+    length: int,
     noise_mat: list,
     reference: list,
-    length: int,
     a1: float,
     a2: float,
     a3: float,
     ff: int,
+    coefficients: list,
 ) -> Callable:
     """Calculate and apply an optimum DPLMS filter to the waveform.
 
@@ -25,8 +26,8 @@ def dplms(
     penalized coefficients [DPLMS]_.
 
     .. [DPLMS] V. D'Andrea et al. “Optimum Filter Synthesis with DPLMS
-        Method for Energy Reconstruction” In: arXiv (Sept. 2022).
-        https://arxiv.org/abs/2209.09863
+        Method for Energy Reconstruction” Eur. Phys. J. C 83, 149 (2023).
+        https://doi.org/10.1140/epjc/s10052-023-11299-z
 
     Note
     ----
@@ -50,6 +51,8 @@ def dplms(
         penalized coefficient for the zero area matrix
     ff
         flat top length for the reference signal
+    coefficients
+        filter coefficients (if given avoid filter calculation)
 
 
     JSON Configuration Example
@@ -72,53 +75,70 @@ def dplms(
     if length <= 0:
         raise DSPFatal("The length of the filter must be positive")
 
-    noise_mat = np.array(noise_mat)
-    reference = np.array(reference)
+    if len(coefficients) > 0:
 
-    if length != noise_mat.shape[0]:
-        raise DSPFatal(
-            "The length of the filter is not consistent with the noise matrix"
-        )
+        if length != len(coefficients):
+            raise DSPFatal("The length and the provided filter are not matching")
 
-    if len(reference) <= 0:
-        raise DSPFatal("The length of the reference signal must be positive")
-
-    if a1 <= 0:
-        raise DSPFatal("The penalized coefficient for the noise must be positive")
-
-    if a2 <= 0:
-        raise DSPFatal("The penalized coefficient for the reference must be positive")
-
-    if a3 <= 0:
-        raise DSPFatal("The penalized coefficient for the zero area must be positive")
-
-    if ff <= 0:
-        raise DSPFatal("The penalized coefficient for the ref matrix must be positive")
-
-    # reference matrix
-    ssize = len(reference)
-    flo = int(ssize / 2 - length / 2)
-    fhi = int(ssize / 2 + length / 2)
-    ref_mat = np.zeros([length, length])
-    ref_sig = np.zeros([length])
-    if ff == 0:
-        ff = [0]
-    elif ff == 1:
-        ff = [-1, 0, 1]
+        x = np.array(coefficients)
     else:
-        raise DSPFatal("The penalized coefficient for the ref matrix must be 0 or 1")
-    for i in ff:
-        ref_mat += np.outer(reference[flo + i : fhi + i], reference[flo + i : fhi + i])
-        ref_sig += reference[flo + i : fhi + i]
-    ref_mat /= len(ff)
+        noise_mat = np.array(noise_mat)
+        reference = np.array(reference)
 
-    # filter calculation
-    mat = a1 * noise_mat + a2 * ref_mat + a3 * np.ones([length, length])
-    x = np.linalg.solve(mat, ref_sig)
-    y = np.convolve(reference, np.flip(x), mode="valid")
-    maxy = np.amax(y)
-    x /= maxy
-    y /= maxy
+        if length != noise_mat.shape[0]:
+            raise DSPFatal(
+                "The length of the filter is not consistent with the noise matrix"
+            )
+
+        if len(reference) <= 0:
+            raise DSPFatal("The length of the reference signal must be positive")
+
+        if a1 <= 0:
+            raise DSPFatal("The penalized coefficient for the noise must be positive")
+
+        if a2 <= 0:
+            raise DSPFatal(
+                "The penalized coefficient for the reference must be positive"
+            )
+
+        if a3 <= 0:
+            raise DSPFatal(
+                "The penalized coefficient for the zero area must be positive"
+            )
+
+        if ff <= 0:
+            raise DSPFatal(
+                "The penalized coefficient for the ref matrix must be positive"
+            )
+
+        # reference matrix
+        ssize = len(reference)
+        flo = int(ssize / 2 - length / 2)
+        fhi = int(ssize / 2 + length / 2)
+        ref_mat = np.zeros([length, length])
+        ref_sig = np.zeros([length])
+        if ff == 0:
+            ff = [0]
+        elif ff == 1:
+            ff = [-1, 0, 1]
+        else:
+            raise DSPFatal(
+                "The penalized coefficient for the ref matrix must be 0 or 1"
+            )
+        for i in ff:
+            ref_mat += np.outer(
+                reference[flo + i : fhi + i], reference[flo + i : fhi + i]
+            )
+            ref_sig += reference[flo + i : fhi + i]
+        ref_mat /= len(ff)
+
+        # filter calculation
+        mat = a1 * noise_mat + a2 * ref_mat + a3 * np.ones([length, length])
+        x = np.linalg.solve(mat, ref_sig)
+        y = np.convolve(reference, np.flip(x), mode="valid")
+        maxy = np.amax(y)
+        x /= maxy
+        y /= maxy
 
     @guvectorize(
         ["void(float32[:], float32[:])", "void(float64[:], float64[:])"],
