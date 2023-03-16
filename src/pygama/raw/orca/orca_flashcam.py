5beade3d7a873fc355f8912fb0b26fc25bb9e75f
jasondet <jasondet@gmail.com>
2023-03-16 20:27:46 +0100

    fix timestamp

--- src/pygama/raw/orca/orca_flashcam.py
+++ src/pygama/raw/orca/orca_flashcam.py
@@ -681,11 +681,9 @@ class ORFlashCamWaveformDecoder(OrcaDecoder):
         tbl["dr_stop_pps"].nda[ii] = int_packet[offset + 2]
         tbl["dr_stop_ticks"].nda[ii] = int_packet[offset + 3]
         tbl["dr_maxticks"].nda[ii] = int_packet[offset + 4]
-        tbl["deadtime"].nda[ii] = np.float64(
-            int_packet[offset + 2] - int_packet[offset]
-        ) + np.float64(int_packet[offset + 3] - int_packet[offset + 1]) / np.float64(
-            int_packet[offset + 4]
-        )
+        dr_dpps = np.float64(int_packet[offset + 2] - int_packet[offset]) 
+        dr_dticks = np.float64(int_packet[offset + 3] - int_packet[offset + 1])
+        tbl["deadtime"].nda[ii] = dr_dpps + dr_dticks / np.float64(int_packet[offset + 4])
 
         # set the event number and clock counters
         offset += 5
@@ -710,14 +708,14 @@ class ORFlashCamWaveformDecoder(OrcaDecoder):
             self.header.fc_gps[fcid] = 0
         if self.header.fc_gps[fcid] == 0:
             toff = (
-                np.float64(packet[offset + 0]) + np.float64(packet[offset + 1]) * 1e-6
+                np.float64(tbl["mu_offset_sec"].nda[ii] + np.float64(tbl["mu_offset_usec"].nad[ii]) * 1e-6
             )
         else:
-            if tbl["delta_mu_usec"].nda[ii] >= self.header.fc_gps[fcid]:
+            if tbl["abs_delta_mu_usec"].nda[ii] >= self.header.fc_gps[fcid]:
                 log.warning(
-                    f"delta {tbl['delta_mu_usec'].nda[ii]} > gps drift allowance {self.header.fc_gps[fcid]}"
+                    f"delta {tbl['abs_delta_mu_usec'].nda[ii]} > gps drift allowance {self.header.fc_gps[fcid]}"
                 )
-            toff = np.float64(packet[offset + 2])
+            toff = np.float64(tbl["to_master_sec"].nad[ii])
         tbl["timestamp"].nda[ii] = tstamp + toff
 
         # set the fpga baseline/energy and waveform
