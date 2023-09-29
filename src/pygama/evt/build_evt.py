1a966de2186ab238cea2873a25fe3d9ad72664fc
Patrick Krause <patrick.krause@tum.de>
2023-09-29 14:05:36 +0200

    3x speedup and added LAr classifier module

--- src/pygama/evt/build_evt.py
+++ src/pygama/evt/build_evt.py
@@ -37,7 +37,7 @@ def num_and_pars(value: str,par_dic: dict):
                 pass
     return value
 
-def evaluate_expression(f_evt:str,f_hit:str, f_dsp: str, chns: list, mode: str, expr: str, para: dict = None, defv = np.nan, getch: bool = False) -> np.ndarray:
+def evaluate_expression(f_evt:str,f_hit:str, f_dsp: str, chns: list, mode: str, expr: str, para: dict = None, defv = np.nan, nrows: int = None) -> np.ndarray:
      """
      Evaluates the expression defined by the user across all channels according to the mode
      Parameters
@@ -67,59 +67,47 @@ def evaluate_expression(f_evt:str,f_hit:str, f_dsp: str, chns: list, mode: str, 
         Only affects "first", "last" modes. In that cases the rawid of the resulting values channel is returned as well.
      """
      #define dimension of output array
-     out = np.zeros(store.LH5Store().read_n_rows(chns[0]+"/dsp/",f_dsp))
-     out[:] = defv
+     n = nrows if nrows is not None else store.LH5Store().read_n_rows(chns[0]+"/dsp/",f_dsp)
+     out = np.full(n,defv,dtype=type(defv))
      out_chs =  np.zeros(len(out),dtype=int)
      
+     # find parameters in evt file or in parameters 
+     exprl = re.findall(r"[a-zA-Z_$][\w$]*",expr)
+     var_ph = {}
+     if os.path.exists(f_evt):var_ph = store.load_nda(f_evt,[e.split('/')[-1] for e in store.ls(f_evt) if e.split('/')[-1] in exprl])
+     if para: var_ph = var_ph | para
+     
      if mode == "func":
-        exprl = re.findall(r"[a-zA-Z_$][\w$]*",expr)
-        var = {}
-        if os.path.exists(f_evt):var = store.load_nda(f_evt,[e.split('/')[-1] for e in store.ls(f_evt) if e.split('/')[-1] in exprl])
-        if para: var = var | para
-        
         # evaluate expression
         func, params = expr.split('(')
-        params = params[:-1].split(',')
-        params = [f_hit,f_dsp,chns]+[num_and_pars(e,var) for e in params]
+        params = [f_hit,f_dsp,chns]+[num_and_pars(e,var_ph) for e in params[:-1].split(',')]
 
         # load function dynamically
         p,m = func.rsplit('.',1)
-        mod = import_module(p)
-        met = getattr(mod,m)
-
-        return met(*params)
+        met = getattr(import_module(p),m)
+        out = met(*params)
 
      else:
+        # evaluate operator in mode
+        ops = re.findall(r'([<>]=?|==)', mode)
+        ch_comp = None
+        if os.path.exists(f_evt) and mode in store.ls(f_evt):
+            ch_comp = store.load_nda(f_evt,[mode])[mode]
         for ch in chns:
-            #find all potential variables 
-            exprl = re.findall(r"[a-zA-Z_$][\w$]*",expr)
-
-            # find fields in either dsp, hit, evt or parameters, prepare evaluation
-            evt_dic = {}
-            if os.path.exists(f_evt): evt_dic = store.load_nda(f_evt,[e.split('/')[-1] for e in store.ls(f_evt) if e.split('/')[-1] in exprl])
-            hit_dic = store.load_nda(f_hit,[e.split('/')[-1] for e in store.ls(f_hit,ch+"/hit/") if e.split('/')[-1] in exprl],ch+"/hit/")
+            # find fields in either dsp, hit
+            var = store.load_nda(f_hit,[e.split('/')[-1] for e in store.ls(f_hit,ch+"/hit/") if e.split('/')[-1] in exprl],ch+"/hit/")
             dsp_dic = store.load_nda(f_dsp,[e.split('/')[-1] for e in store.ls(f_dsp,ch+"/dsp/") if e.split('/')[-1] in exprl],ch+"/dsp/")
-            
-            var= hit_dic | dsp_dic | evt_dic
-            if para: var = var | para
-
+            var= var |dsp_dic | var_ph
+        
             # evaluate expression
             res = eval(expr,var)
-            if not isinstance(res, np.ndarray):
-                res = np.full(len(out),res,dtype=type(res))
+            if not isinstance(res, np.ndarray): res = np.full(len(out),res,dtype=type(res))
 
             # append to out according to mode
-            ops = re.findall(r'([<>]=?|==)', mode)
-            if len(ops)>0:
-                op = ops[0]
-                lim = float(mode.split(op)[-1])
-                limarr = eval("res"+op+"lim",{"res":res,"lim":lim})
-            else:
-                
-                limarr = np.ones(len(res)).astype(bool)
+            if  len(ops)>0: limarr = eval("".join(["res",ops[0],"lim"]),{"res":res,"lim":float(mode.split(ops[0])[-1])})
+            else: limarr = np.ones(len(out)).astype(bool)
             if "first" in mode:
-                outt = np.zeros(len(out))
-                outt[:] = np.inf
+                outt = np.full(len(out),np.inf)
                 t0 = store.load_nda(f_dsp,["tp_0_est"],ch+"/dsp/")["tp_0_est"]
                 out = np.where((t0<outt) & (limarr),res,out)
                 out_chs = np.where((t0<outt) & (limarr),int(ch[2:]),out_chs)
@@ -128,30 +116,23 @@ def evaluate_expression(f_evt:str,f_hit:str, f_dsp: str, chns: list, mode: str, 
                 outt = np.zeros(len(out))
                 t0 = store.load_nda(f_dsp,["tp_0_est"],ch+"/dsp/")["tp_0_est"]
                 out = np.where((t0>outt) & (limarr),res,out)
-                out_chs = np.where((t0<outt) & (limarr),int(ch[2:]),out_chs)
+                out_chs = np.where((t0>outt) & (limarr),int(ch[2:]),out_chs)
                 outt = np.where((t0>outt) & (limarr),t0,outt)
             elif "tot" in mode:
-                if ch == chns[0]: out[:] = 0
                 if res.dtype == bool: res = res.astype(int)
                 out += np.where(limarr,res,out)
             elif mode == "any":
-                    if ch == chns[0]:
-                        out = out.astype(bool)
                     if res.dtype != bool: res = res.astype(bool)
                     out = out | res
             elif mode == "all":
-                    if ch == chns[0]: 
-                        out = out.astype(bool)
                     if res.dtype != bool: res = res.astype(bool)
                     out = out & res
-            elif mode in store.ls(f_evt):
-                ch_comp = store.load_nda(f_evt,[mode])[mode]
+            elif ch_comp is not None:
                 out = np.where(int(ch[2:]) == ch_comp,res,out)
             else:
                 raise ValueError(mode + " not a valid mode")
 
-     if getch: return out, out_chs
-     else: return out       
+     return out, out_chs     
              
 def build_evt(
        f_dsp: str,
@@ -248,7 +229,7 @@ def build_evt(
         
 
     # do operations
-    first_iter = True
+    first_iter,nrows = True,None
     log.info(f"Applying'{len(tbl_cfg['operations'].keys())} operations' to dsp file {f_dsp} and hit file {f_hit} to create evt file {f_evt}")
     for k, v in tbl_cfg['operations'].items(): 
          log.debug("Processing field" + k)
@@ -266,44 +247,32 @@ def build_evt(
                 lh5_file=f_evt,
                 wo_mode=wo_mode #if first_iter else "append"
             )
-            continue
-             
-         if isinstance(v['channels'],str): chns_e = chns[v['channels']]
-         elif isinstance(v['channels'],list): chns_e = list(itertools.chain.from_iterable( [chns[e] for e in v['channels']]))
-
-         pars = None
-         defaultv = np.nan
-         if "parameters" in v.keys(): pars = v['parameters']
-         if "initial" in v.keys() and not v['initial'] == "np.nan" : defaultv = v['initial']
-
-         if "get_ch" in v.keys():
-              if "first" in v['mode'] or "last" in v['mode']:
-                   res, chs = evaluate_expression(f_evt,f_hit,f_dsp,chns_e,v['mode'],v['expression'],pars,defaultv, v["get_ch"])
-                   lstore.write_object(
-                        obj=Array(res),
-                        name= k,
-                        lh5_file=f_evt,
-                        wo_mode=wo_mode #if first_iter else "append"
-                   )
-                   lstore.write_object(
-                        obj=Array(chs),
-                        name= k+"_id",
-                        lh5_file=f_evt,
-                        wo_mode=wo_mode #if first_iter else "append"
-                   )
-                   
-              else:
-                  raise ValueError("get_ch can be only applied to first and last modes")
-                
          else:
-            res = Array(evaluate_expression(f_evt,f_hit,f_dsp,chns_e,v['mode'],v['expression'],pars,defaultv))
+            if isinstance(v['channels'],str): chns_e = chns[v['channels']]
+            elif isinstance(v['channels'],list): chns_e = list(itertools.chain.from_iterable( [chns[e] for e in v['channels']]))
 
+            pars, defaultv = None , np.nan
+            if "parameters" in v.keys(): pars = v['parameters']
+            if "initial" in v.keys() and not v['initial'] == "np.nan" : defaultv = v['initial']
+
+            res,chs = evaluate_expression(f_evt,f_hit,f_dsp,chns_e,v['mode'],v['expression'],pars,defaultv,nrows)
             lstore.write_object(
-                obj=res,
-                name= k,
-                lh5_file=f_evt,
-                wo_mode=wo_mode #if first_iter else "append"
+                    obj=Array(res),
+                    name= k,
+                    lh5_file=f_evt,
+                    wo_mode=wo_mode
             )
+
+            # if get_ch true flag in a first/last mode operation also obtain channel field 
+            if "get_ch" in v.keys() and ("first" in v['mode'] or "last" in v['mode']) and v["get_ch"]:
+                lstore.write_object(
+                    obj=Array(chs),
+                    name= k+"_id",
+                    lh5_file=f_evt,
+                    wo_mode=wo_mode
+                )
+
          if first_iter: first_iter = False
+         if not nrows: nrows = len(res)
 
     log.info("Done")
--- src/pygama/evt/modules/spm.py
+++ src/pygama/evt/modules/spm.py
@@ -42,3 +42,42 @@ def get_majority(f_hit,f_dsp,chs,lim,trgr,tdefault,tmin,tmax):
         chmaj = np.where(chsum>lim,1,0)
         maj = maj + chmaj
     return maj
+
+def get_etc(f_hit,f_dsp,chs,lim,trgr,tdefault,tmin,tmax,swin,trail):
+        predf = store.load_nda(f_hit, ["energy_in_pe",'timestamp'],chs[0]+"/hit/")
+    
+        peshape = (predf["energy_in_pe"]).shape
+        # 1D = channel, 2D = event num, 3D = array per event
+        pes=np.zeros([len(chs),peshape[0],peshape[1]])
+        times = np.zeros([len(chs),peshape[0],peshape[1]])
+
+        tge = np.where(np.isnan(trgr),tdefault,trgr)
+        tmi = tge - tmin
+        tma = tge + tmax
+        for i in range(len(chs)):
+            df =store.load_nda(f_hit, ["energy_in_pe",'trigger_pos','timestamp'],chs[i]+"/hit/")
+            mask =  (df["trigger_pos"]<tma[:,None]/16) & (df["trigger_pos"]>tmi[:,None]/16) & (df["energy_in_pe"] > lim)
+            pe=df["energy_in_pe"]
+            time = df["trigger_pos"]*16
+        
+            pe= np.where(mask,pe,np.nan)
+            time= np.where(mask,time,np.nan)
+
+            pes[i] = pe
+            times[i] = time
+        
+        outi = None
+        if trail >0:
+            t1d = np.nanmin(times,axis=(0,2))
+            if trail == 2: t1d[t1d>tge] = tge[t1d>tge]
+            tt = t1d[:,None]
+            outi = np.where(np.nansum(np.where((times >= tt),pes,0),axis=(0,2)) > 0,
+                            np.nansum(np.where((times >= tt) & (times < tt+swin),pes,0),axis=(0,2))/np.nansum(np.where((times >= tt),pes,0),axis=(0,2)),
+                            np.nansum(np.where((times >= tt),pes,0),axis=(0,2)))
+            return outi
+            
+        else:
+            outi = np.where(np.nansum(pes,axis=(0,2)) > 0,
+                            np.nansum(np.where((times >= tge[:,None]) & (times <= tge[:,None]+swin),pes,0),axis=(0,2))/np.nansum(np.where((times >= tge[:,None]),pes,0),axis=(0,2)),
+                            np.nansum(pes,axis=(0,2)))
+            return outi
