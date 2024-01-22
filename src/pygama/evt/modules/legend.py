b5240f4a558b8642575aeb0b53478d94b0aa3303
Patrick Krause <patrick.krause@tum.de>
2024-01-22 18:39:13 +0100

    allow passing of env vars in legend meta module

--- src/pygama/evt/modules/legend.py
+++ src/pygama/evt/modules/legend.py
@@ -3,13 +3,15 @@ Module provides LEGEND internal functions
 """
 from importlib import import_module
 
+from lgdo.lh5 import utils
+
 
 def metadata(params: dict) -> list:
     # only import legend meta data when needed.
     # LEGEND collaborators can use the meta keyword
     # While for users w/o access to the LEGEND meta data this is still working
     lm = import_module("legendmeta")
-    lmeta = lm.LegendMetadata(path=params["meta_path"])
+    lmeta = lm.LegendMetadata(path=utils.expand_path(params["meta_path"]))
     chmap = lmeta.channelmap(params["time_key"])
 
     tmp = [
