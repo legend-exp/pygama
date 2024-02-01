10e403c40c5442f36cec97a317d7bccfbf840c20
ggmarshall <george.marshall.20@ucl.ac.uk>
2024-02-01 18:07:00 +0100

    change load_dfs to new read and change plotting to no longer give warnings

--- src/pygama/pargen/extract_tau.py
+++ src/pygama/pargen/extract_tau.py
@@ -26,7 +26,7 @@ import pygama.pargen.dsp_optimize as opt
 import pygama.pargen.energy_optimisation as om
 
 log = logging.getLogger(__name__)
-
+sto = lh5.LH5Store()
 
 def load_data(
     raw_file: list[str],
@@ -36,8 +36,7 @@ def load_data(
     threshold: int = 5000,
     wf_field: str = "waveform",
 ) -> lgdo.Table:
-    sto = lh5.LH5Store()
-    df = lh5.load_dfs(raw_file, ["daqenergy", "timestamp"], lh5_path)
+    df = sto.read(lh5_path, raw_file, field_mask=["daqenergy", "timestamp"])[0].view_as('pd')
 
     if pulser_mask is None:
         pulser_props = cts.find_pulser_properties(df, energy="daqenergy")
@@ -142,8 +141,8 @@ def get_decay_constant(
         )
         axins.axvline(high_bin, color="red")
         axins.set_xlim(bins[in_min], bins[in_max])
-        labels = ax.get_xticklabels()
-        ax.set_xticklabels(labels=labels, rotation=45)
+        ax.set_xticks(ax.get_xticks())
+        ax.set_xticklabels(labels=ax.get_xticklabels(), rotation=45)
         out_plot_dict["slope"] = fig
         if display > 1:
             plt.show()
