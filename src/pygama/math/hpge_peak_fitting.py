31a29465386baeff5d4e5344e017b3f48492956e
ggmarshall <george.marshall.20@ucl.ac.uk>
2024-04-07 13:22:53 +0200

    more samples for bootstrap

--- src/pygama/math/hpge_peak_fitting.py
+++ src/pygama/math/hpge_peak_fitting.py
@@ -170,7 +170,7 @@ def hpge_peak_mode(mu, sigma, htail, tau, cov = None):
         #nsig set to 1, hstep+nbkg set to 0
         pars = np.array([1, mu, sigma, htail, tau,0,0])
         rng = np.random.default_rng(1)
-        par_b = rng.multivariate_normal(pars, cov, size=100)
+        par_b = rng.multivariate_normal(pars, cov, size=10000)
         modes = np.array([hpge_peak_mode(p[1],p[2],p[3],p[4]) for p in par_b])
         mode_err_boot = np.nanstd(modes, axis=0)
 
