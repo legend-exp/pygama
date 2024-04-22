92c0833c1a657c56481b12414580bf091b8213e2
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-04-22 13:04:50 +0200

    update to tests

--- src/pygama/evt/modules/cross_talk.py
+++ src/pygama/evt/modules/cross_talk.py
@@ -9,7 +9,7 @@ from lgdo.lh5 import LH5Store
 
 from pygama.evt import utils
 
-def cross_talk_corrected_energy_awkard_slow(energies:ak.Array,rawids:ak.Array,matrix:dict,allow_non_existing:bool=True):
+def cross_talk_corrected_energy_awkard_slow(energies:ak.Array,rawids:ak.Array,matrix:dict,allow_non_existing:bool=True,threshold:float=None):
     """
     Function to perform the cross talk correction on awkward arrays of energy and rawid.
     The energies are first sorted from largest to smallest, a term is then added to the
@@ -27,11 +27,12 @@ def cross_talk_corrected_energy_awkard_slow(energies:ak.Array,rawids:ak.Array,ma
         - matrix (dict)      : python dictonary of the cross talk correction matrix
         - allow_non_existing (bool): A boolean to control what happens if a rawid is not present
             in the matrix, if True, this matrix element is set to 0, if False an exception is raised.
+        - threshold (float)  : energy threshold below which hits are not used to correct the other hits
     Returns:
         ak.Array of corrected energies
 
     """
-
+  
     # some exceptions
     # check types
     if not isinstance(energies, ak.Array):
@@ -47,14 +48,13 @@ def cross_talk_corrected_energy_awkard_slow(energies:ak.Array,rawids:ak.Array,ma
         raise TypeError("allow_non_existing must be a Boolean")
 
     # first check that energies and rawids have the same dimensions
-    if (ak.all(ak.num(energies,axis=-1)!=ak.num(rawids,axis=-1))):
+    if (ak.any(ak.num(energies,axis=-1)!=ak.num(rawids,axis=-1))):
         raise ValueError("Error: the length of each subarray of energies and rawids must be equal")
     
     if (ak.num(energies,axis=-2)!=ak.num(rawids,axis=-2)):
         raise ValueError("Error: the number of energies is not equal to rawids")
     
     # check that the matrix elements exist
-
     for c1 in np.unique(ak.flatten(rawids).to_numpy()):
         if c1 not in matrix.keys():
 
@@ -83,18 +83,23 @@ def cross_talk_corrected_energy_awkard_slow(energies:ak.Array,rawids:ak.Array,ma
 
     ## sort the energies and rawids
     args = ak.argsort(energies,ascending=False)
-
     energies= energies[args]
     rawids = rawids[args]
 
+
+
+    ## run the correction  
+    ## --------------------
     energies_corrected = []
+
     ## we should try to speed this up
     for energy_vec_tmp,rawid_vec_tmp in zip(energies,rawids):
         
         energies_corrected_tmp = list(energy_vec_tmp)
         for id_main, (energy_main,rawid_main) in enumerate(zip(energy_vec_tmp,rawid_vec_tmp)):
+            if (threshold is not None and energy_main<threshold):
+                break
             for id_other, (energy_other,rawid_other) in enumerate(zip(energy_vec_tmp,rawid_vec_tmp)):
-
                 if (id_main!=id_other):
                     energies_corrected_tmp[id_other]+=matrix[rawid_main][rawid_other]*energy_main
         
@@ -106,27 +111,4 @@ def cross_talk_corrected_energy_awkard_slow(energies:ak.Array,rawids:ak.Array,ma
 
 
 
-
-def get_energy_corrected(f_hit:str,f_dsp:str,f_tcm:str,hit_group:str,dsp_group:str,tcm_group:str,tcm_id_table_pattern:str,channels:list,
-                        cross_talk_matrix:str,energy_variable:str)->VectorOfVectors:
-    """
-    Function to compute cross-talk corrected energies.
-    Parameters
-        - f_hit (str): The path to the hit tier file
-        - f_dsp (str): The path the digital signal proccesing (dsp) file
-        - f_tcm (str): The path to the time coincidence map (tcm)
-        - tcm_group (str): LH5 root group in `tcm` file.
-        - dsp_group (str): LH5 root group in `dsp` file.
-        - hit_group (str): LH5 root group in `hit` file.
-        - cross_talk_matrix: The path to the cross talk matrix
-        - channels (list): List of channels to proccess
-        - tcm_id_table_pattern: (str): pattern to format tcm id values to table name in higher tiers. Must have one
-        placeholder which is the `tcm` id.
-        - cross_talk_matrix (str): Path to the cross talk matrix - currently a JSON file (should be updated?)
-        - energy_variable (str): hit tier energy variable to correct
-    Returns:
-        VectorofVectors of the corrected enerrgies
-    """
-
-    raise NotImplementedError("error cross talk correction is not yet implemented")
 
--- tests/evt/test_cross_talk.py
+++ tests/evt/test_cross_talk.py
@@ -1,6 +1,7 @@
 import awkward as ak
 from pygama.evt.modules import cross_talk
 import pytest
+import numpy as np
 def test_cross_talk_corrected_energy_awkard_slow():
 
     
@@ -20,12 +21,12 @@ def test_cross_talk_corrected_energy_awkard_slow():
 
     # filter some values from energy first so each event has a different size
     with pytest.raises(ValueError):
-        cross_talk.cross_talk_corrected_energy_awkard_slow(energies_test[energies_test!=1000],rawid_test,matrix,True)
+        cross_talk.cross_talk_corrected_energy_awkard_slow(energies_test[energies_test<600],rawid_test,matrix,True)
 
     ## checks on the matrix
     # first check if the matrix has empty elements an exception is raised if allow_non_existing is false
-    matrix_not_full={1:{1:1.00, 2:0.01, 3:0.02, 4:0.00},
-                    2:{1:0.01, 2:1.00, 3:0},
+    matrix_not_full={1:{1:1.00,  3:0.02, 4:0.00},
+                    2:{2:1.00, 3:0},
                     3:{1:0.02, 2:0.00, 3:1.00, 4:0.01},
                     4:{1:0.00, 3:0.01, 4:1.00}
                     }  
@@ -44,13 +45,19 @@ def test_cross_talk_corrected_energy_awkard_slow():
     ## now check the result returned (given no exceptions)
     ### 1st event is two channels 1% cross talk and [1000,200] energy so we expect Ecorr  = [1002,210] 
     ### 2nd event one channel so the energy shouldnt change
-    ### 3rd event  3 channels [500,3000,100] energy ch1 has 1% ct to ch2 and 2% to ch3 so we will have
-    ### E=[500,3005,102] then ch2 has 1% cross talk to ch1 and  0 to ch3
-    ### E= [503,3005,102] finally ch3 has 2% cross talk with ch1
-    ### E=[505,3005,102]
- 
+    ### 3rd event  3 channels [500,3000,100] energy ch2 has 1% ct to ch1 and 0% to ch3 so we will have
+    ### E= [530,3000,100]
+    ### next correct E2--> 3005, E3-->110]
+    ## finally correct from E3, 530-> 532 , 3005->3005
     energy_corr =cross_talk.cross_talk_corrected_energy_awkard_slow(energies_test,rawid_test,matrix,True)   
 
     assert np.all(energy_corr[0].to_numpy()==np.array([1002,210]))
     assert np.all(energy_corr[1].to_numpy()==np.array([100]))
-    assert np.all(energy_corr[2].to_numpy()==np.array([505,3005,102]))
+    assert np.allclose(energy_corr[2].to_numpy(),np.array([532.,3005.,110.]))
+
+
+    ## test with a threshold of 1 MeV
+
+    energy_corr_threshold =cross_talk.cross_talk_corrected_energy_awkard_slow(energies_test,rawid_test,matrix,True,threshold=1000)   
+
+    assert np.allclose(energy_corr_threshold[2].to_numpy(),np.array([530,3000,100]))
