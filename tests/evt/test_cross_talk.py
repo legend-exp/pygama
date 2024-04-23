5fcb198dbed487fb429ee244e8c730c0889235c4
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-04-23 11:14:02 +0200

    update to tests to use xtalk not cross_talk

--- tests/evt/test_cross_talk.py
+++ tests/evt/test_cross_talk.py
@@ -2,10 +2,10 @@ import awkward as ak
 import numpy as np
 import pytest
 
-from pygama.evt.modules import cross_talk
+from pygama.evt.modules import xtalk
 
 
-def test_cross_talk_corrected_energy_awkard_slow():
+def test_xtalk_corrected_energy_awkard_slow():
 
     energies_test = ak.Array([[1000, 200], [100], [500, 3000, 100]])
     rawid_test = ak.Array([[1, 2], [3], [1, 2, 3]])
@@ -20,13 +20,13 @@ def test_cross_talk_corrected_energy_awkard_slow():
 
     # if rawid and energies have different shapes (juts the first entry)
     with pytest.raises(ValueError):
-        cross_talk.cross_talk_corrected_energy_awkard_slow(
+        xtalk.xtalk_corrected_energy_awkard_slow(
             ak.Array([[1000, 200]]), rawid_test, matrix, True
         )
 
     # filter some values from energy first so each event has a different size
     with pytest.raises(ValueError):
-        cross_talk.cross_talk_corrected_energy_awkard_slow(
+        xtalk.xtalk_corrected_energy_awkard_slow(
             energies_test[energies_test < 600], rawid_test, matrix, True
         )
 
@@ -40,7 +40,7 @@ def test_cross_talk_corrected_energy_awkard_slow():
     }
 
     with pytest.raises(ValueError):
-        cross_talk.cross_talk_corrected_energy_awkard_slow(
+        xtalk.xtalk_corrected_energy_awkard_slow(
             energies_test, rawid_test, matrix_not_full, False
         )
 
@@ -51,7 +51,7 @@ def test_cross_talk_corrected_energy_awkard_slow():
         4: {1: 0.00, 2: 0.01, 3: 0.01, 4: 1.00},
     }
     with pytest.raises(ValueError):
-        cross_talk.cross_talk_corrected_energy_awkard_slow(
+        xtalk.xtalk_corrected_energy_awkard_slow(
             energies_test, rawid_test, matrix_not_sym, True
         )
 
@@ -62,7 +62,7 @@ def test_cross_talk_corrected_energy_awkard_slow():
     ### E= [530,3000,100]
     ### next correct E2--> 3005, E3-->110]
     ## finally correct from E3, 530-> 532 , 3005->3005
-    energy_corr = cross_talk.cross_talk_corrected_energy_awkard_slow(
+    energy_corr = xtalk.xtalk_corrected_energy_awkard_slow(
         energies_test, rawid_test, matrix, True
     )
 
@@ -72,8 +72,45 @@ def test_cross_talk_corrected_energy_awkard_slow():
 
     ## test with a threshold of 1 MeV
 
-    energy_corr_threshold = cross_talk.cross_talk_corrected_energy_awkard_slow(
+    energy_corr_threshold = xtalk.xtalk_corrected_energy_awkard_slow(
         energies_test, rawid_test, matrix, True, threshold=1000
     )
 
     assert np.allclose(energy_corr_threshold[2].to_numpy(), np.array([530, 3000, 100]))
+
+
+def test_manipulate_xtalk_matrix():
+
+    test_matrix={
+        "V02160A": {"V02160B": 0.1},
+        "V02160B": {"V02160A": 0.1}
+    }    
+    test_positive={
+        "V02160A": {"V02160B": 0.3},
+        "V02160B": {"V02160A": 0.3}
+    }    
+    test_small={
+        "V02160A": {"V02160B": 0.01},
+        "V02160B": {"V02160A": 0.01}
+    }    
+
+    test_bad_channel={
+        "AAAAAAA": {"BBBBBBB": 0.1},
+        "BBBBBBB": {"AAAAAAA": 0.1}
+    }   
+    matrix_rawids = xtalk.manipulate_xtalk_matrix(test_matrix,None,True)
+
+    ## check names are converted to rawid ok
+    
+    assert matrix_rawids["ch1104000"]["ch1104001"]==test_matrix["V02160A"]["V02160B"]
+
+    matrix_comb = xtalk.manipulate_xtalk_matrix(test_matrix,test_positive,True)
+    assert matrix_comb["ch1104000"]["ch1104001"]==-0.3
+
+    matrix_comb_small = xtalk.manipulate_xtalk_matrix(test_matrix,test_small,True)
+    assert matrix_comb_small["ch1104000"]["ch1104001"]==0.1
+
+    ## check if the matrix contains some non-existing channel the right exception is raised
+    with pytest.raises(ValueError):
+        xtalk.manipulate_xtalk_matrix(test_bad_channel,None,True)
+
