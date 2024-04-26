db26b05fb948e2e12cd58500665f552ca37e6fda
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-04-26 14:07:20 +0200

    simplification of code + building directly energy array

--- src/pygama/evt/modules/geds.py
+++ src/pygama/evt/modules/geds.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import json
 from collections.abc import Sequence
 
 import numpy as np
@@ -17,121 +16,76 @@ def apply_xtalk_correction(
     tcm: utils.TCMData,
     table_names: Sequence[str],
     *,
-    energy_observable: types.VectorOfVectors,
-    rawids: types.VectorOfVectors,
+    uncalibrated_energy_name: str,
+    calibrated_energy_name: str,
     threshold: float = None,
     xtalk_matrix_filename: str,
-    positive_xtalk_matrix_filename: str = None,
     xtalk_rawid_name: str = "xtalk_rawids",
-    xtalk_matrix_name: str = "xtalk_matrix",
-    positive_xtalk_matrix_name: str = None,
-    is_json_input_file: bool = True,
-    det_names: bool = False,
-    energy_observable_negative: types.VectorOfVectors = None,
-    use_slow_correction: bool = True,
+    xtalk_matrix_name: str = "xtalk_matrix_negative",
+    positive_xtalk_matrix_name: str = "xtalk_matrix_positive",
 ) -> types.VectorOfVectors:
     """Applies the cross-talk correction to the energy observable.
-    The format of `xtalk_matrix_filename` should be currently be a path to a JSON file.
+    The format of `xtalk_matrix_filename` should be currently be a path to a lh5 file.
 
-    The correction is appplied recursively, the energies are sorted and then the correction
-    is applied from the largest to smallest (above the threshold).
+    The correction is appplied using matrix algebra for all triggers above the threshold.
 
     Parameters
     ----------
     datainfo, tcm, table_names
         positional arguments automatically supplied by :func:`.build_evt`.
-    energy_observable
-        array of energy values to correct, one event per row. The detector
-        identifier is stored in `rawids`, which has the same layout.
-    rawids
-        array of detector identifiers for each energy in `energy_observable`.
+    uncalibrated_energy_name
+        name of the pulse parameter for uncalibrated energy to be gathered, optionally prefixed by tier
+        name (e.g. ``hit.cusp_Emax``). If no tier is specified, it defaults
+        to ``hit``.
+    calibrated_energy_name
+        name of the pulse parameter for calibrated energy to be gathered, optionally prefixed by tier
+        name (e.g. ``hit.cusp_Emax``). If no tier is specified, it defaults
+        to ``hit``.
     threshold
         threshold used for xtalk correction, hits below this energy will not
         be used to correct the other hits.
     xtalk_matrix_filename
         name of the file containing the xtalk matrices.
-    positive_xtalk_matrix_filename
-        name of the file containing the positive xtalk matrices.
     xtalk_matrix_name
         name of the lh5 object containing the xtalk matrix
     positive_xtalk_matrix_name
         name of the lh5 object containing the positive polarity xtalk matrix
     xtalk_matrix_rawids
         name of the lh5 object containing the name of the rawids
-    is_json_input_file
-        boolean to say the xtalk matrix is stored as json file
-    det_names
-        bool to say the xtalk matrices use the detector names (default False)
-    energy_observable_negative
-        array of negative energy values to correct, one event per row. The detector
-        identifier is stored in `rawids`, which has the same layout. Default None
-    use_slow_correction
-        boolean flag to use the slow xtalk correction (with loops)
     """
-    # read json file to a dict
-    if is_json_input_file:
-        try:
-            with open(xtalk_matrix_filename) as file:
-                xtalk_matrix = json.load(file)
-        except FileNotFoundError:
-            raise ValueError(
-                f"path to x-talk matrix {xtalk_matrix_filename} does not exist"
-            )
 
-        positive_xtalk_matrix = None
-        if positive_xtalk_matrix_filename is not None:
-            try:
-                with open(xtalk_matrix_filename) as file:
-                    positive_xtalk_matrix = json.load(file)
-            except FileNotFoundError:
-                raise ValueError(
-                    f"path to x-talk matrix {positive_xtalk_matrix_filename} does not exist"
-                )
+    # read lh5 files to numpy
+    xtalk_matrix_numpy = lh5.read_as(xtalk_matrix_name, xtalk_matrix_filename, "np")
+    xtalk_matrix_rawids = lh5.read_as(xtalk_rawid_name, xtalk_matrix_filename, "np")
+    positive_xtalk_matrix_numpy = lh5.read_as(
+        positive_xtalk_matrix_name, xtalk_matrix_filename, "np"
+    )
 
-    else:
-        # read lh5 files to numpy
-        xtalk_matrix_numpy = lh5.read_as(xtalk_matrix_name, xtalk_matrix_filename, "np")
-        xtalk_matrix_rawids = lh5.read_as(xtalk_rawid_name, xtalk_matrix_filename, "np")
+    # Combine positive and negative matrixs
+    # Now the matrix should have negative values corresponding to negative cross talk
+    # and positive values corresponding to positive cross talk .
+    # we also set nan to 0 and we transpose so that the row corresponds to response and column trigger
+    xtalk_matrix = np.nan_to_num(
+        np.where(
+            abs(xtalk_matrix_numpy) > abs(positive_xtalk_matrix_numpy),
+            xtalk_matrix_numpy,
+            positive_xtalk_matrix_numpy,
+        ),
+        0,
+    ).T
 
-        if positive_xtalk_matrix_filename is not None:
-            positive_xtalk_matrix_numpy = lh5.read_as(
-                positive_xtalk_matrix_name, positive_xtalk_matrix_filename, "np"
-            )
-            positive_xtalk_matrix_rawids = lh5.read_as(
-                xtalk_rawid_name, positive_xtalk_matrix_filename, "np"
-            )
+    uncalibrated_energy_array = xtalk.build_energy_array(
+        uncalibrated_energy_name, tcm, datainfo, xtalk_matrix_rawids
+    )
+    calibrated_energy_array = xtalk.build_energy_array(
+        calibrated_energy_name, tcm, datainfo, xtalk_matrix_rawids
+    )
 
-            if np.any(xtalk_matrix_rawids != positive_xtalk_matrix_rawids):
-                raise ValueError(
-                    "Positive and Negative cross talk matrix must have the same rawids"
-                )
-
-    if use_slow_correction:
-        xtalk_matrix = xtalk.numpy_to_dict(xtalk_matrix_numpy, xtalk_matrix_rawids)
-
-        if positive_xtalk_matrix_filename is not None:
-            positive_xtalk_matrix = xtalk.numpy_to_dict(
-                positive_xtalk_matrix_numpy, positive_xtalk_matrix_rawids
-            )
-
-        xtalk_matrix = xtalk.manipulate_xtalk_matrix(
-            xtalk_matrix, positive_xtalk_matrix, det_names
-        )
-
-        # do the correction
-        energies_corr = xtalk.xtalk_corrected_energy_awkward_slow(
-            energies=energy_observable.view_as("ak"),
-            rawids=rawids.view_as("ak"),
-            matrix=xtalk_matrix,
-            allow_non_existing=False,
-            threshold=threshold,
-        )
-    else:
-        raise NotImplementedError(
-            "Error fast (matrix based ) xtalk correction isnt implemented yet"
-        )
+    energies_corr = xtalk.xtalk_corrected_energy(
+        uncalibrated_energy_array, calibrated_energy_array, xtalk_matrix, threshold
+    )
 
     # return the result as LGDO
     return types.VectorOfVectors(
-        energies_corr, attrs=utils.copy_lgdo_attrs(energy_observable)
+        energies_corr, attrs=utils.copy_lgdo_attrs(uncalibrated_energy_name)
     )
--- src/pygama/evt/modules/xtalk.py
+++ src/pygama/evt/modules/xtalk.py
@@ -4,118 +4,67 @@ Module for cross talk correction of energies.
 
 import awkward as ak
 import numpy as np
-from legendmeta import LegendMetadata
+from lgdo import lh5
+
+from pygama.lgdo import ls
+
+from .. import utils
 
 
-def numpy_to_dict(array: np.ndarray, rawids: np.ndarray) -> dict:
-    """Converts a 2D numpy array with a 1D array of rawids (channel names) to a dictonary
+def build_energy_array(
+    observable: str, tcm: utils.TCMData, datainfo: utils.DataInfo, rawids: np.ndarray
+) -> np.ndarray:
+    """
+    Builds the array of energies for the cross talk correction
     Parameters
     ----------
-    array
-        2D numpy array
+    observable
+        name of the pulse parameter to be gathered, optionally prefixed by tier
+        name (e.g. ``hit.cuspEmax_ctc_cal``). If no tier is specified, it defaults
+        to ``hit``.
+    datainfo
+        utils.DataInfo object
+    tcm
+        time-coincidence map object
     rawids
-        numpy array of associated channels / rawids
+        list of channel rawids from the cross talk matrix.
     """
 
-    if array.ndim != 2:
-        raise ValueError("array must be 2D ")
-    if rawids.ndim != 1:
-        raise ValueError("rawids must be 1D")
-    if len(rawids) != array.shape[0] or len(rawids) != array.shape[1]:
-        raise ValueError(
-            "The number of elements of rawids should match the shape of 'array'"
-        )
+    # build the energy arrays
+    p = observable.split(".")
+    tier = p[0] if len(p) > 1 else "hit"
+    column = p[1] if len(p) > 1 else p[0]
 
-    out_dict = {}
-    for i, row in enumerate(array):
-        out_dict[f"ch{rawids[i]}"] = {}
-        for j, value in enumerate(row):
-            out_dict[f"ch{rawids[i]}"][f"ch{rawids[j]}"] = value
+    table_fmt = datainfo._asdict()[tier].table_fmt
+    group = datainfo._asdict()[tier].group
+    file = datainfo._asdict()[tier].file
 
-    return out_dict
+    # initialise the output object
+    energies_out = np.full((len(rawids), np.max(ak.flatten(tcm.idx)) + 1), np.nan)
 
+    # parse observables string. default to hit tier
+    keys = ls(file)
 
-def manipulate_xtalk_matrix(
-    xtalk_matrix: dict, positive_xtalk_matrix: dict = None, det_names: bool = False
-):
-    """
-    Function to read in and manipulate the cross talk matrix.
-    Parameters
-    ----------
-    xtalk_matrix_filename (str)
-         Path to the xtalk matrix
-    positive_xtalk_matrix_filename (str)
-        Path to the positive polarity cross talk matrix
-    det_names
-        bool to convert det names to rawids
+    for idx_chan, channel in enumerate(rawids):
 
-    """
+        # get the event indexs
+        table_id = utils.get_tcm_id_by_pattern(table_fmt, channel)
+        idx_events = ak.to_numpy(ak.flatten(tcm.idx[tcm.id == table_id]))
 
-    if det_names is True:
-        xtalk_matrix = convert_matrix_det_names_to_rawid(xtalk_matrix)
-
-    # read the positive matrix
-    if positive_xtalk_matrix is not None:
-
-        if det_names is True:
-            positive_xtalk_matrix = convert_matrix_det_names_to_rawid(
-                positive_xtalk_matrix
+        # read the energy data
+        if f"ch{channel}" in keys:
+            data = lh5.read_as(
+                f"ch{channel}/{group}/{column}", file, idx=idx_events, library="np"
             )
+            energies_out[idx_chan][idx_events] = data
 
-        # merge +ive and -ive matrix
-        matrix_merge = {}
-        for key_row, row in xtalk_matrix.items():
-            matrix_merge[key_row] = {}
-            for key_col, data in row.items():
-
-                positive_xtalk = positive_xtalk_matrix[key_row][key_col]
-                negative_xtalk = data
-
-                if positive_xtalk > negative_xtalk:
-                    matrix_merge[key_row][key_col] = -positive_xtalk
-                else:
-                    matrix_merge[key_row][key_col] = negative_xtalk
-
-        xtalk_matrix = matrix_merge
-
-    return xtalk_matrix
-
-
-def convert_matrix_det_names_to_rawid(matrix: dict) -> dict:
-    """
-    Converts a cross talk matrix with keys of detector names to one with keys rawids
-    Parameters
-    ----------
-    matrix
-        dictonary of the cross talk matrix
-    """
-    metadb = LegendMetadata()
-    chmap = metadb.channelmap("20230323T000000Z")
-
-    geds_mapping = {
-        _name: f"ch{_dict['daq']['rawid']}"
-        for _name, _dict in chmap.items()
-        if chmap[_name]["system"] == "geds"
-    }
-    matrix_conv = {}
-    for key_row, row in matrix.items():
-
-        if key_row not in geds_mapping.keys():
-            raise ValueError(f"channel {key_row} doesnt have a valid rawid")
-
-        matrix_conv[geds_mapping[key_row]] = {}
-        for key_col, data in row.items():
-
-            if key_col not in geds_mapping.keys():
-                raise ValueError(f"channel {key_col} doesnt have a valid rawid")
-
-            matrix_conv[geds_mapping[key_row]][geds_mapping[key_col]] = data
-
-    return matrix_conv
+    # transpose to return object where row is events and column rawid idx
+    return energies_out.T
 
 
 def xtalk_corrected_energy(
-    energies: np.ndarray,
+    uncalibrated_energies: np.ndarray,
+    calibrated_energies: np.ndarray,
     matrix: np.ndarray,
     threshold: float = None,
 ):
@@ -125,13 +74,15 @@ def xtalk_corrected_energy(
     2. All energy less than the threshold are set to 0
     3. The correction is computed as:
     .. math::
-        E_{\text{cor},i}=E_{j}\times M_{ij}
+        E_{\text{cor},i}=-\times M_{i,j}E_{j}
 
-    where $M_{ij}$ is the cross talk matrix.
+    where $M_{i,j}$ is the cross talk matrix element where i is response and j trigger channel.
     Parameters
     ----------
-    energies
-        2D numpy array of the energies in each event, the row corresponds to an event and the column the rawid
+    uncalibrated_energies
+        2D numpy array of the uncalibrated energies in each event, the row corresponds to an event and the column the rawid
+    calibrated_energies
+        2D numpy array of the calibrated energies in each event, the row corresponds to an event and the column the rawid
     matrix
         2D numpy array of the cross talk correction matrix, the indexs should correspond to rawids (with same mapping as energies)
     threshold
@@ -140,136 +91,12 @@ def xtalk_corrected_energy(
     """
     # check input shapes and sizes
 
-    if energies.ndim != 2:
-        raise ValueError("energies must be a 2D array")
+    uncalibrated_energies_no_nan = np.nan_to_num(uncalibrated_energies, 0)
+    calibrated_energies_no_nan = np.nan_to_num(calibrated_energies, 0)
 
-    if matrix.ndim != 2:
-        raise ValueError("xtalk matrix must be a 2D array")
+    energies_threshold = np.where(
+        calibrated_energies_no_nan < threshold, 0, uncalibrated_energies_no_nan
+    )
 
-    if matrix.shape[0] != energies.shape[1]:
-        raise ValueError(
-            "the energy vector must have the smae size as the cross talk matrix"
-        )
-
-    energies_threshold = energies * (energies > threshold)
-
-    energies_correction = np.matmul(matrix,energies_threshold.T).T
-    return energies + energies_correction
-
-
-def xtalk_corrected_energy_awkward_slow(
-    energies: ak.Array,
-    rawids: ak.Array,
-    matrix: dict,
-    allow_non_existing: bool = True,
-    threshold: float = None,
-):
-    """
-    Function to perform the cross talk correction on awkward arrays of energy and rawid.
-    The energies are first sorted from largest to smallest, a term is then added to the
-        other energies of the cross talk matrix element multipled by the largest energy.
-    .. math::
-        E_{i,\text{cor}} = E_{1}\times M[c_i,c_1]+E_{i},
-        where $c_i$ is the raw-id of the $i$th energy.
-
-    This process is repeated recursively for the 2nd largest energy etc.
-    This implementation is called 'slow' since it uses loops over events and energies
-
-    Parameters
-        - energies (ak.Array): array of energies
-        - rawids   (ak.Array): array of rawids
-        - matrix (dict)      : python dictonary of the cross talk correction matrix
-        - allow_non_existing (bool): A boolean to control what happens if a rawid is not present
-            in the matrix, if True, this matrix element is set to 0, if False an exception is raised.
-        - threshold (float)  : energy threshold below which hits are not used to correct the other hits
-    Returns:
-        ak.Array of corrected energies
-
-    """
-
-    # some exceptions
-    # check types
-    if not isinstance(energies, ak.Array):
-        raise TypeError("energies must be an awkward array")
-
-    if not isinstance(rawids, ak.Array):
-        raise TypeError("rawids must be an awkward array")
-
-    if not isinstance(matrix, dict):
-        raise TypeError("matrix must be a python dictonary")
-
-    if not isinstance(allow_non_existing, bool):
-        raise TypeError("allow_non_existing must be a Boolean")
-
-    # first check that energies and rawids have the same dimensions
-    if ak.any(ak.num(energies, axis=-1) != ak.num(rawids, axis=-1)):
-        raise ValueError(
-            "Error: the length of each subarray of energies and rawids must be equal"
-        )
-
-    if ak.num(energies, axis=-2) != ak.num(rawids, axis=-2):
-        raise ValueError("Error: the number of energies is not equal to rawids")
-
-    # check that the matrix elements exist
-    for c1 in np.unique(ak.flatten(rawids).to_numpy()):
-        if c1 not in matrix.keys():
-
-            if allow_non_existing is True:
-                matrix[c1] = {}
-            else:
-                raise ValueError(
-                    f"Error allow_non_existing is set to False and {c1} isnt present in the matrix"
-                )
-
-        for c2 in np.unique(ak.flatten(rawids).to_numpy()):
-            if c1 == c2:
-                continue
-            else:
-                if c2 not in matrix[c1].keys():
-                    if allow_non_existing is True:
-                        matrix[c1][c2] = 0
-                    else:
-                        raise ValueError(
-                            f"Error allow_non_existing is set to False and {c2} isnt present in the matrix[{c1}]"
-                        )
-
-    # add a check that the matrix is symmetric
-
-    for c1 in matrix.keys():
-        for c2 in matrix[c1].keys():
-            if abs(matrix[c1][c2] - matrix[c2][c1]) > 1e-6:
-                raise ValueError(
-                    f"Error input cross talk matrix is not symmetric for {c1},{c2}"
-                )
-
-    # sort the energies and rawids
-    args = ak.argsort(energies, ascending=False)
-    energies = energies[args]
-    rawids = rawids[args]
-
-    # run the correction
-    # --------------------
-
-    energies_corrected = []
-
-    # we should try to speed this up
-    for energy_vec_tmp, rawid_vec_tmp in zip(energies, rawids):
-
-        energies_corrected_tmp = list(energy_vec_tmp)
-        for id_main, (energy_main, rawid_main) in enumerate(
-            zip(energy_vec_tmp, rawid_vec_tmp)
-        ):
-            if threshold is not None and energy_main < threshold:
-                break
-            for id_other, (_energy_other, rawid_other) in enumerate(
-                zip(energy_vec_tmp, rawid_vec_tmp)
-            ):
-                if id_main != id_other:
-                    energies_corrected_tmp[id_other] += (
-                        matrix[rawid_main][rawid_other] * energy_main
-                    )
-
-        energies_corrected.append(energies_corrected_tmp)
-
-    # convert to awkward array and unsort
-    return ak.Array(energies_corrected)[args]
+    energies_correction = -np.matmul(matrix, energies_threshold.T).T
+    return uncalibrated_energies + energies_correction
