ebed9fd4854ef74185b544d42e4c1db6679545dc
iguinn <iguinn@email.unc.edu>
2024-11-01 11:27:56 -0700

    Fixed math defaults test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ import pygama.utils as pgu
 
 def test_math_numba_defaults():
     assert pgu.numba_math_defaults_kwargs.fastmath
-    assert pgu.numba_math_defaults_kwargs.parallel
+    assert not pgu.numba_math_defaults_kwargs.parallel
 
     pgu.numba_math_defaults.fastmath = False
-    assert ~pgu.numba_math_defaults.fastmath
+    assert not pgu.numba_math_defaults.fastmath
