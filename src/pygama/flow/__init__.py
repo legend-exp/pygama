f24d918a7abb8ad59840c3ae5d04f113c3252091
iguinn <guinnis@ornl.gov>
2025-11-17 09:09:11 -0800

    Added query_meta

--- src/pygama/flow/__init__.py
+++ src/pygama/flow/__init__.py
@@ -2,7 +2,8 @@
 High-level data flow handling routines.
 """
 
+from .query_meta import query_metadata
 from .data_loader import DataLoader
 from .file_db import FileDB
 
-__all__ = ["DataLoader", "FileDB"]
+__all__ = ["DataLoader", "FileDB", "query_metadata"]
