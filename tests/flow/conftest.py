3f5c4be3627e44a01908039dd9e0203532b7525a
Schwarz, Mario <schmar9@gmx.de>
2026-02-16 16:44:09 +0100

    fixed data_loader and filedb tests for new test data by restricting use to p03/r001

--- tests/flow/conftest.py
+++ tests/flow/conftest.py
@@ -14,7 +14,9 @@ def test_filedb(lgnd_test_data):
         config = json.load(f)
 
     config["data_dir"] = lgnd_test_data.get_path("lh5/prod-ref-l200/generated/tier")
-    return FileDB(config)
+    db = FileDB(config, scan=False)
+    db.scan_files(["cal/p03/r001","phy/p03/r001"])
+    return db
 
 
 @pytest.fixture(scope="module")
@@ -23,6 +25,7 @@ def test_filedb_full(lgnd_test_data):
         config = json.load(f)
 
     config["data_dir"] = lgnd_test_data.get_path("lh5/prod-ref-l200/generated/tier")
-    db = FileDB(config)
+    db = FileDB(config, scan=False)
+    db.scan_files(["cal/p03/r001","phy/p03/r001"])
     db.scan_tables_columns()
     return db
